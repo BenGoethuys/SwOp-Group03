7107d9773abc460bfbcddfec3a89ad494362097c
Mathias Dekempeneer <mathiasdekempeneer@gmail.com>
2016-02-17 14:07:22 +0100

    Test case folder

---
+++ BugTrap/BugTrap/src/bugdomain/MainTest.java
@@ -0,0 +1,14 @@
+package bugdomain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
