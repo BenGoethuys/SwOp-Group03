ad9974e85a02312d8808143e9fdb504a95bf67c1
VincentDerk <vincent.derkinderen@student.kuleuven.be>
2016-02-17 16:33:54 +0100

    Update

--- BugTrap/BugTrap/Tests/bugdomain/TestBla.java
+++ BugTrap/BugTrap/Tests/bugdomain/TestBla.java
@@ -6,9 +6,9 @@ import org.junit.Test;
 
 public class TestBla {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
 
 }
