13f13db9be1e0b0300d2badd9ecfe5f69137a81e
KwintenB <kwinten.buytaert@hotmail.com>
2016-02-18 00:47:00 +0100

    added comments/function headings in AbstracSystem

--- BugTrap/BugTrap/src/bugdomain/AbstractSystem.java
+++ BugTrap/BugTrap/src/bugdomain/AbstractSystem.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 /**
  * Created by Kwinten on 17/02/2016.
+ *
+ * TODO if parent is pushed down to subclass, methods can be pushed down to...
  */
 public abstract class AbstractSystem {
 
@@ -13,6 +15,17 @@ public abstract class AbstractSystem {
     private AbstractSystem parent;
     private ArrayList<Subsystem> childs;
 
+    /**
+     * CAN BE PUSHED DOWN IN HIERARCHY --> remove because only necessary in Subsystem
+     *
+     * This constructor is used for elements of the subclass Subsyste, since they  have a parent.
+     * @param version The versionID (of that type) of this element.
+     * @param name The string name for this element.
+     * @param description The string description of this element.
+     * @param parent The parent (a Project or Subsystem) of this element.
+     * @throws NullPointerException if the versionID is null.
+     * @throws IllegalArgumentException if one of the String arguments is invalid.
+     */
     public AbstractSystem(VersionID version, String name, String description, AbstractSystem parent)throws NullPointerException, IllegalArgumentException{
         this(version, name, description);
         if (! this.isValidName(name, parent)) {
@@ -21,6 +34,14 @@ public abstract class AbstractSystem {
         this.setParent(parent);
     }
 
+    /**
+     * This constructor is used for all elements of type 'AbstracSystem', although possibly indirect.
+     * @param version The versionID (of that type) of this element.
+     * @param name The string name for this element.
+     * @param description The string description of this element.
+     * @throws NullPointerException if the versionID is null.
+     * @throws IllegalArgumentException if one of the String arguments is invalid.
+     */
     public AbstractSystem(VersionID version, String name, String description)throws NullPointerException, IllegalArgumentException{
         // not yet implemented
         if (version != null) {
@@ -43,7 +64,7 @@ public abstract class AbstractSystem {
     // Exceptions werpen in de isValid in plaats van in de constructor, en de isValid in de set...
 
     /**
-     *
+     * This is a getter for the version variable.
      * @return The versionID of the project.
      */
     public VersionID getVersionID() {
@@ -52,16 +73,14 @@ public abstract class AbstractSystem {
 
     /**
      * Sets the versionID of the project to the given versionID.
-     *
-     * @param version
-     *            The versionID of the project.
+     * @param version The versionID of the project.
      */
     private void setVersionID(VersionID version) {
         this.version = version;
     }
 
     /**
-     *
+     * This is a getter for the name variable.
      * @return The name of the project.
      */
     public String getName() {
@@ -70,22 +89,34 @@ public abstract class AbstractSystem {
 
     /**
      * Sets the name of the project to the given name.
-     *
-     * @param name
-     *            The name of the project.
+     * @param name The name of the project.
      */
     private void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * This method checks the validity of the given name.
+     * @param name The string argument to used as name.
+     * @return true if the name is not an empty string or null.
+     */
     protected boolean isValidName(String name){
-        return (name != "");
+        return (name != "" && name != null);
     }
 
+    /**
+     * CAN BE PUSHED DOWN IN HIERARCHY --> remove because only necessary in Subsystem
+     *
+     * This is an abstract heading for the function that chekcs
+     * the validity of the given name, in combination with its parent.
+     * @param name The string argument to be used as name.
+     * @param parent The parent of the element to be named.
+     * @return true is valid.
+     */
     protected abstract boolean isValidName(String name, AbstractSystem parent);
 
     /**
-     *
+     * This is a getter for the description variable.
      * @return The description of the project.
      */
     public String getDescription() {
@@ -93,18 +124,19 @@ public abstract class AbstractSystem {
     }
 
     /**
-     * Sets the description of the project to the given description.
-     *
-     * @param description
-     *            The description of the project.
+     * Sets the description of the AbstractSystem to the given description.
+     * @param description The description of the project.
      */
     private void setDescription(String description) {
         this.description = description;
     }
 
     /**
+     * CAN BE PUSHED DOWN IN HIERARCHY--> only necessary for Subsystem
      *
-     * @param parent
+     * Sets the parent of the AbstractSystem to the given parent, if valid.
+     * Only elements of subclass subsystems have a parent different from null.
+     * @param parent The given parent of the AbstractSystem
      */
     private void setParent(AbstractSystem parent){
         if (isValidParent(parent)){
@@ -113,11 +145,22 @@ public abstract class AbstractSystem {
             parent.addChild((Subsystem)this);
         }
     }
+
+    /**
+     * CAN BE PUSHED DOWN IN HIERARCHY --> remove because only necessary in Subsystem
+     *
+     * This abstract method is the heading for a validity check on the parent.
+     * @param parent The given parent to be checked.
+     * @return true if valid.
+     */
     protected abstract boolean isValidParent(AbstractSystem parent);
 
     /**
+     * CAN BE PUSHED DOWN IN HIERARCHY
      *
-     * @return
+     * This is a getter for the parent of the AbstractSystem.
+     * A project is it's own parent.
+     * @return The parent of an element with type Subclass or the Project.
      */
     protected AbstractSystem getParent(){
         if (this.parent != null){
@@ -127,20 +170,38 @@ public abstract class AbstractSystem {
         }
     }
 
+    /**
+     * This method returns the head of the subsystem tree structure.
+     * This is an element from the type Project and can be recognised by his self reference in getParent().
+     * @return the Project to which al the subsystems are linked.
+     */
+    protected Project getParentProject(){
+        AbstractSystem localParent = this.getParent();
+        AbstractSystem localGrandParent = localParent.getParent();
+        while (localParent != localGrandParent){
+            localParent = localGrandParent;
+            localGrandParent = localParent.getParent();
+        }
+        //only an element of type project has itself as parent value
+        return (Project)localParent;
+    }
+
+    /**
+     * A getter for the arraylist of childs.
+     * @return an arraylist of childs.
+     */
     protected ArrayList<Subsystem> getChilds(){
         return this.childs;
     }
 
+    /**
+     * This method sets the child variable to the given child.
+     * A child is of type Subsystem.
+     * @param child The given subsystem to set as child.
+     */
     protected void addChild(Subsystem child){
         this.getChilds().add(child);
     }
 
-    protected Project getParentProject(){
-        AbstractSystem localParent = this.getParent();
-        while (localParent != null){
-            localParent = localParent.getParent();
-        }
-        //only an element of type project has null as parent value
-        return (Project)localParent;
-    }
+
 }
