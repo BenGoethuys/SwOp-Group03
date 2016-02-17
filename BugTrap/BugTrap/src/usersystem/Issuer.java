65c7f7180fd8acf1df71fc4223f029f9b1ccbc78
Mathias Dekempeneer <mathiasdekempeneer@gmail.com>
2016-02-17 15:18:25 +0100

    Developer extends the Issuer

--- BugTrap/BugTrap/src/usersystem/Issuer.java
+++ BugTrap/BugTrap/src/usersystem/Issuer.java
@@ -2,4 +2,12 @@ package usersystem;
 
 public class Issuer extends User {
 
+	public Issuer(String uniqueUsername, String firstName, String middleName, String lastName) {
+		super(uniqueUsername, firstName, middleName, lastName);
+	}
+
+	public Issuer(String uniqueUsername, String firstName, String lastName) {
+		super(uniqueUsername, firstName, lastName);
+	}
+
 }
