80aa743ccf969db825a5a14f3a417c01cbb44b21
Mathias Dekempeneer <mathiasdekempeneer@gmail.com>
2016-02-17 17:56:21 +0100

    Initialize test cases for VersionID class

---
+++ BugTrap/BugTrap/Tests/bugdomain/VersionIDTest.java
@@ -0,0 +1,168 @@
+/**
+ * 
+ */
+package bugdomain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Mathias
+ *
+ */
+public class VersionIDTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link bugdomain.VersionID#VersionID(int, int, int)}.
+	 */
+	@Test
+	public void testVersionID() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bugdomain.VersionID#getFirstNb()}.
+	 */
+	@Test
+	public void testGetFirstNb() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bugdomain.VersionID#getSecondNb()}.
+	 */
+	@Test
+	public void testGetSecondNb() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bugdomain.VersionID#getThirdNb()}.
+	 */
+	@Test
+	public void testGetThirdNb() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bugdomain.VersionID#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bugdomain.VersionID#compareTo(bugdomain.VersionID)}.
+	 */
+	@Test
+	public void testCompareTo() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#Object()}.
+	 */
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#getClass()}.
+	 */
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#hashCode()}.
+	 */
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#equals(java.lang.Object)}.
+	 */
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#clone()}.
+	 */
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#toString()}.
+	 */
+	@Test
+	public void testToString1() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#notify()}.
+	 */
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#notifyAll()}.
+	 */
+	@Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait(long)}.
+	 */
+	@Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait(long, int)}.
+	 */
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait()}.
+	 */
+	@Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#finalize()}.
+	 */
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
