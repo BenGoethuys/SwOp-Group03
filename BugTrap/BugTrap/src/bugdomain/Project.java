fdc2d511ac57933471522ed435d7081d3dafb092
Mathias Dekempeneer <mathiasdekempeneer@gmail.com>
2016-02-18 08:29:24 +0100

    Small modifications

--- BugTrap/BugTrap/src/bugdomain/Project.java
+++ BugTrap/BugTrap/src/bugdomain/Project.java
@@ -7,7 +7,7 @@ public class Project extends AbstractSystem {
 	private Date creationDate;
 	private Date startDate;
 
-	//testing comments because you know
+	// testing comments because you know
 
 	/**
 	 * Creates a project with a given versionID, name, description, creationDate
@@ -23,11 +23,14 @@ public class Project extends AbstractSystem {
 	 *            The creation date of the project.
 	 * @param startDate
 	 *            The start date of the project.
-	 * @throws NullPointerException if the versionID is null.
-	 * @throws IllegalArgumentException if one of the String arguments or dates is invalid.
+	 * @throws NullPointerException
+	 *             if the versionID is null.
+	 * @throws IllegalArgumentException
+	 *             if one of the String arguments or dates is invalid.
 	 */
-	public Project(VersionID version, String name, String description, Date creationDate, Date startDate) throws IllegalArgumentException{
-		super(version,name,description);
+	public Project(VersionID version, String name, String description, Date creationDate, Date startDate)
+			throws IllegalArgumentException {
+		super(version, name, description);
 		if (isValidStartDate()) {
 			setCreationDate(creationDate);
 			setStartDate(startDate);
@@ -40,12 +43,17 @@ public class Project extends AbstractSystem {
 	 * CAN BE REMOVED WITH PARENT PUSHED DOWN IN HIERARCHY
 	 *
 	 * This method checks the validity of the given name with the given parent.
-	 * A Project is initialised with null as parent value and will return itself as parent.
-	 * @param name The string argument to be used as name.
-	 * @param parent The parent of the element to be named.
-     * @return true if the name is valid on its own and the parent is null or itself
-     */
-	protected boolean isValidName(String name, AbstractSystem parent){
+	 * A Project is initialised with null as parent value and will return itself
+	 * as parent.
+	 * 
+	 * @param name
+	 *            The string argument to be used as name.
+	 * @param parent
+	 *            The parent of the element to be named.
+	 * @return true if the name is valid on its own and the parent is null or
+	 *         itself
+	 */
+	protected boolean isValidName(String name, AbstractSystem parent) {
 		if (this.isValidName(name)) {
 			return parent == null || parent == this;
 		}
@@ -54,6 +62,7 @@ public class Project extends AbstractSystem {
 
 	/**
 	 * This method checks the validity of the startdate.
+	 * 
 	 * @return True if creation date <= start date.
 	 */
 	private boolean isValidStartDate() {
@@ -65,6 +74,7 @@ public class Project extends AbstractSystem {
 
 	/**
 	 * This i a getter for the startdate variable.
+	 * 
 	 * @return The start date of the project.
 	 */
 	public Date getStartDate() {
@@ -73,7 +83,9 @@ public class Project extends AbstractSystem {
 
 	/**
 	 * Sets the start date of the project to the given date.
-	 * @param startDate  The start date of the project.
+	 * 
+	 * @param startDate
+	 *            The start date of the project.
 	 */
 	private void setStartDate(Date startDate) {
 		this.startDate = startDate;
@@ -81,6 +93,7 @@ public class Project extends AbstractSystem {
 
 	/**
 	 * This is a getter for the CreationDate variable.
+	 * 
 	 * @return The creation date of the project.
 	 */
 	public Date getCreationDate() {
@@ -89,21 +102,27 @@ public class Project extends AbstractSystem {
 
 	/**
 	 * Sets the creation date of the project to the given date.
-	 * @param creationDate The creation date of the project.
+	 * 
+	 * @param creationDate
+	 *            The creation date of the project.
 	 */
 	private void setCreationDate(Date creationDate) {
 		this.creationDate = creationDate;
 	}
 
 	/**
-	 * This method checks the validity of a given parent.
-	 * A project's parent is to be kept at null. It will however return itself as parent in the getter.
-	 * @param parent The given parent to be checked.
+	 * This method checks the validity of a given parent. A project's parent is
+	 * to be kept at null. It will however return itself as parent in the
+	 * getter.
+	 * 
+	 * @param parent
+	 *            The given parent to be checked.
 	 * @return false
-     */
-	 protected boolean isValidParent(AbstractSystem parent){
-		 //null is de standaarwaarde voor een niet geïmplementeerde parent van project. Indien we true zouden teruggeven,
-		 //kan er een nullpointer exception ontstaan bij setParent.
-		 return false;
-	 }
+	 */
+	protected boolean isValidParent(AbstractSystem parent) {
+		// null is de standaarwaarde voor een niet geïmplementeerde parent van
+		// project. Indien we true zouden teruggeven,
+		// kan er een nullpointer exception ontstaan bij setParent.
+		return false;
+	}
 }
--- BugTrap/BugTrap/src/usersystem/User.java
+++ BugTrap/BugTrap/src/usersystem/User.java
@@ -9,182 +9,205 @@ package usersystem;
  */
 public class User {
 
-    /**
-     * Create a {@link User} with a unique username, a firstName, middelName and
-     * lastName.
-     *
-     * @param uniqueUsername The unique username of this user.
-     * @param firstName The first name of this user.
-     * @param middleName The middle name of this user.
-     * @param lastName The last name of this user.
-     *
-     * @throws IllegalArgumentException When any of the arguments is invalid.
-     * @see isValidUniqueUsername(String uniqueUsername)
-     * @see isValidFirstName(String firstName)
-     * @see isValidMiddleName(String middleName)
-     * @see isValidLastName(String lastName)
-     */
-    public User(String uniqueUsername, String firstName, String middleName, String lastName) throws IllegalArgumentException {
-        setUniqueUsername(uniqueUsername);
-        setFirstName(firstName);
-        setMiddleName(middleName);
-        setLastName(lastName);
-    }
+	/**
+	 * Create a {@link User} with a unique username, a firstName, middelName and
+	 * lastName.
+	 *
+	 * @param uniqueUsername
+	 *            The unique username of this user.
+	 * @param firstName
+	 *            The first name of this user.
+	 * @param middleName
+	 *            The middle name of this user.
+	 * @param lastName
+	 *            The last name of this user.
+	 *
+	 * @throws IllegalArgumentException
+	 *             When any of the arguments is invalid.
+	 * @see isValidUniqueUsername(String uniqueUsername)
+	 * @see isValidFirstName(String firstName)
+	 * @see isValidMiddleName(String middleName)
+	 * @see isValidLastName(String lastName)
+	 */
+	public User(String uniqueUsername, String firstName, String middleName, String lastName)
+			throws IllegalArgumentException {
+		setUniqueUsername(uniqueUsername);
+		setFirstName(firstName);
+		setMiddleName(middleName);
+		setLastName(lastName);
+	}
 
-    /**
-     * Create a {@link User} with a unique username, a firstName and lastName.
-     *
-     * @param uniqueUsername The unique username of this user.
-     * @param firstName The first name of this user.
-     * @param lastName The last name of this user.
-     *
-     * @throws IllegalArgumentException When any of the arguments is invalid.
-     * @see isValidUniqueUsername(String uniqueUsername)
-     * @see isValidFirstName(String firstName)
-     * @see isValidLastName(String lastName)
-     */
-    public User(String uniqueUsername, String firstName, String lastName) {
-        this(uniqueUsername, firstName, "", lastName);
-    }
+	/**
+	 * Create a {@link User} with a unique username, a firstName and lastName.
+	 *
+	 * @param uniqueUsername
+	 *            The unique username of this user.
+	 * @param firstName
+	 *            The first name of this user.
+	 * @param lastName
+	 *            The last name of this user.
+	 *
+	 * @throws IllegalArgumentException
+	 *             When any of the arguments is invalid.
+	 * @see isValidUniqueUsername(String uniqueUsername)
+	 * @see isValidFirstName(String firstName)
+	 * @see isValidLastName(String lastName)
+	 */
+	public User(String uniqueUsername, String firstName, String lastName) {
+		this(uniqueUsername, firstName, "", lastName);
+	}
 
-    private String uniqueUsername;
-    private String firstName;
-    private String middleName;
-    private String lastName;
+	private String uniqueUsername;
+	private String firstName;
+	private String middleName;
+	private String lastName;
 
-    /**
-     * Get the unique username of this {@link User}
-     *
-     * @return The last name of this user.
-     */
-    public String getUniqueUsername() {
-        return this.uniqueUsername;
-    }
+	/**
+	 * Get the unique username of this {@link User}
+	 *
+	 * @return The last name of this user.
+	 */
+	public String getUniqueUsername() {
+		return this.uniqueUsername;
+	}
 
-    /**
-     * Set uniqueUsername as the unique username of this user, if it is valid.
-     *
-     * @param uniqueUsername The new unique username.
-     * @throws IllegalArgumentException When the uniqueUsername is not valid.
-     * @see isValidUniqueUsername(String uniqueUsername)
-     */
-    private void setUniqueUsername(String uniqueUsername) throws IllegalArgumentException {
-        if (isValidUniqueUsername(uniqueUsername)) {
-            this.uniqueUsername = uniqueUsername;
-        } else {
-            throw new IllegalArgumentException("Unique username:" + uniqueUsername + " is not a valid unique username.");
-        }
-    }
+	/**
+	 * Set uniqueUsername as the unique username of this user, if it is valid.
+	 *
+	 * @param uniqueUsername
+	 *            The new unique username.
+	 * @throws IllegalArgumentException
+	 *             When the uniqueUsername is not valid.
+	 * @see isValidUniqueUsername(String uniqueUsername)
+	 */
+	private void setUniqueUsername(String uniqueUsername) throws IllegalArgumentException {
+		if (isValidUniqueUsername(uniqueUsername)) {
+			this.uniqueUsername = uniqueUsername;
+		} else {
+			throw new IllegalArgumentException(
+					"Unique username:" + uniqueUsername + " is not a valid unique username.");
+		}
+	}
 
-    /**
-     * Check if uniqueUsername is a valid username. Uniqueness is not checked.
-     *
-     * @param uniqueUsername The username to check.
-     * @return Whether the uniqueUsername is valid.
-     */
-    public boolean isValidUniqueUsername(String uniqueUsername) {
-        return (uniqueUsername != null && !uniqueUsername.equalsIgnoreCase(""));
-    }
+	/**
+	 * Check if uniqueUsername is a valid username. Uniqueness is not checked.
+	 *
+	 * @param uniqueUsername
+	 *            The username to check.
+	 * @return Whether the uniqueUsername is valid.
+	 */
+	public boolean isValidUniqueUsername(String uniqueUsername) {
+		return (uniqueUsername != null && !uniqueUsername.equalsIgnoreCase(""));
+	}
 
-    /**
-     * Get the first name of this {@link User}
-     *
-     * @return The first name of this user.
-     */
-    public String getFirstName() {
-        return this.firstName;
-    }
+	/**
+	 * Get the first name of this {@link User}
+	 *
+	 * @return The first name of this user.
+	 */
+	public String getFirstName() {
+		return this.firstName;
+	}
 
-    /**
-     * Set firstName as the first name of this user, if it is valid.
-     *
-     * @param firstName The new first name.
-     * @throws IllegalArgumentException When the firstName is not valid.
-     * @see isValidFirstName(String firstName)
-     */
-    private void setFirstName(String firstName) throws IllegalArgumentException {
-        if (isValidFirstName(firstName)) {
-            this.firstName = firstName;
-        } else {
-            throw new IllegalArgumentException("First name:" + firstName + " is not a valid first name.");
-        }
-    }
+	/**
+	 * Set firstName as the first name of this user, if it is valid.
+	 *
+	 * @param firstName
+	 *            The new first name.
+	 * @throws IllegalArgumentException
+	 *             When the firstName is not valid.
+	 * @see isValidFirstName(String firstName)
+	 */
+	private void setFirstName(String firstName) throws IllegalArgumentException {
+		if (isValidFirstName(firstName)) {
+			this.firstName = firstName;
+		} else {
+			throw new IllegalArgumentException("First name:" + firstName + " is not a valid first name.");
+		}
+	}
 
-    /**
-     * Check if firstName is a valid first name.
-     *
-     * @param firstName The first name to check.
-     * @return Whether the first name is valid.
-     */
-    public boolean isValidFirstName(String firstName) {
-        return (firstName != null && !firstName.equalsIgnoreCase(""));
-    }
+	/**
+	 * Check if firstName is a valid first name.
+	 *
+	 * @param firstName
+	 *            The first name to check.
+	 * @return Whether the first name is valid.
+	 */
+	public boolean isValidFirstName(String firstName) {
+		return (firstName != null && !firstName.equalsIgnoreCase(""));
+	}
 
-    /**
-     * Get the middle name of this {@link User}
-     *
-     * @return The middle name of this user.
-     */
-    public String getMiddleName() {
-        return this.middleName;
-    }
+	/**
+	 * Get the middle name of this {@link User}
+	 *
+	 * @return The middle name of this user.
+	 */
+	public String getMiddleName() {
+		return this.middleName;
+	}
 
-    /**
-     * Set middleName as the middle name of this user, if it is valid.
-     *
-     * @param middleName The new middle name.
-     * @throws IllegalArgumentException When the middleName is not valid.
-     * @see isValidmiddleName(String middleName)
-     */
-    private void setMiddleName(String middleName) {
-        if (isValidMiddleName(middleName)) {
-            this.middleName = middleName;
-        } else {
-            throw new IllegalArgumentException("Middle name:" + middleName + " is not a valid middle name.");
-        }
-    }
+	/**
+	 * Set middleName as the middle name of this user, if it is valid.
+	 *
+	 * @param middleName
+	 *            The new middle name.
+	 * @throws IllegalArgumentException
+	 *             When the middleName is not valid.
+	 * @see isValidmiddleName(String middleName)
+	 */
+	private void setMiddleName(String middleName) {
+		if (isValidMiddleName(middleName)) {
+			this.middleName = middleName;
+		} else {
+			throw new IllegalArgumentException("Middle name:" + middleName + " is not a valid middle name.");
+		}
+	}
 
-    /**
-     * Check if middle name is a valid middle name.
-     *
-     * @param middleName The middle name to check.
-     * @return Whether the middle name is valid.
-     */
-    public boolean isValidMiddleName(String middleName) {
-        return middleName != null;
-    }
+	/**
+	 * Check if middle name is a valid middle name.
+	 *
+	 * @param middleName
+	 *            The middle name to check.
+	 * @return Whether the middle name is valid.
+	 */
+	public boolean isValidMiddleName(String middleName) {
+		return middleName != null;
+	}
 
-    /**
-     * Get the last name of this {@link User}
-     *
-     * @return The last name of this user.
-     */
-    public String getLastName() {
-        return this.lastName;
-    }
+	/**
+	 * Get the last name of this {@link User}
+	 *
+	 * @return The last name of this user.
+	 */
+	public String getLastName() {
+		return this.lastName;
+	}
 
-    /**
-     * Set lastName as the last name of this user, if it is valid.
-     *
-     * @param lastName The new last name.
-     * @throws IllegalArgumentException When the last name is not valid.
-     * @see isValidLastName(String lastName)
-     */
-    private void setLastName(String lastName) {
-        if (isValidLastName(lastName)) {
-            this.lastName = lastName;
-        } else {
-            throw new IllegalArgumentException("Middle name:" + middleName + " is not a valid middle name.");
-        }
-    }
+	/**
+	 * Set lastName as the last name of this user, if it is valid.
+	 *
+	 * @param lastName
+	 *            The new last name.
+	 * @throws IllegalArgumentException
+	 *             When the last name is not valid.
+	 * @see isValidLastName(String lastName)
+	 */
+	private void setLastName(String lastName) {
+		if (isValidLastName(lastName)) {
+			this.lastName = lastName;
+		} else {
+			throw new IllegalArgumentException("Middle name:" + middleName + " is not a valid middle name.");
+		}
+	}
 
-    /**
-     * Check if lastName is a valid last name.
-     *
-     * @param lastName The last name to check.
-     * @return Whether the lastName is valid.
-     */
-    public boolean isValidLastName(String lastName) {
-        return (lastName != null && !lastName.equals(""));
-    }
+	/**
+	 * Check if lastName is a valid last name.
+	 *
+	 * @param lastName
+	 *            The last name to check.
+	 * @return Whether the lastName is valid.
+	 */
+	public boolean isValidLastName(String lastName) {
+		return (lastName != null && !lastName.equals(""));
+	}
 }
