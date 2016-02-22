8937f2fbc4f5b5f52bcb01e883955172128f8c16
VincentDerk <vincent.derkinderen@student.kuleuven.be>
2016-02-22 17:16:15 +0100

    Updated to test the abstract design of User.

---
+++ BugTrap/BugTrap/Tests/bugtrap03/usersystem/UserTestDummy.java
@@ -0,0 +1,18 @@
+package bugtrap03.usersystem;
+
+/**
+ * {@link User} test class.
+ * 
+ * @author Admin
+ */
+public class UserTestDummy extends User {
+
+    public UserTestDummy(String username, String firstName, String middleName, String lastName) throws IllegalArgumentException {
+        super(username, firstName, middleName, lastName);
+    }
+
+    public UserTestDummy(String username, String firstName, String lastName) throws IllegalArgumentException {
+        super(username, firstName, lastName);
+    }
+
+}
