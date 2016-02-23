034b5720008b9e0aec3d7e33e2075e8d1c3f4b29
VincentDerk <vincent.derkinderen@student.kuleuven.be>
2016-02-24 00:19:48 +0100

    Added doc.

--- BugTrap/BugTrap/src/bugtrap03/usersystem/Administrator.java
+++ BugTrap/BugTrap/src/bugtrap03/usersystem/Administrator.java
@@ -1,13 +1,39 @@
 package bugtrap03.usersystem;
 
+/**
+ *
+ * @author Admin
+ * @version 0.1
+ */
 public class Administrator extends User {
 
-	public Administrator(String uniqueUsername, String firstName, String middleName, String lastName) {
-		super(uniqueUsername, firstName, middleName, lastName);
-	}
+    /**
+     * Create an {@link Administrator} with a specific username, first name, middle name and last
+     * name.
+     *
+     * @param uniqueUsername The username
+     * @param firstName The first name
+     * @param middleName The middle name
+     * @param lastName The last name
+     * @throws IllegalArgumentException When the creator of User throws it.
+     * @see User#User(java.lang.String, java.lang.String, java.lang.String, java.lang.String)
+     */
+    public Administrator(String uniqueUsername, String firstName, String middleName, String lastName) throws IllegalArgumentException {
+        super(uniqueUsername, firstName, middleName, lastName);
+    }
 
-	public Administrator(String uniqueUsername, String firstName, String lastName) {
-		super(uniqueUsername, firstName, lastName);
-	}
+    /**
+     * Create an {@link Administrator} with a specific username, first name and last
+     * name.
+     *
+     * @param uniqueUsername The username
+     * @param firstName The first name
+     * @param lastName The last name
+     * @throws IllegalArgumentException When the creator of User throws it.
+     * @see User#User(java.lang.String, java.lang.String, java.lang.String)
+     */
+    public Administrator(String uniqueUsername, String firstName, String lastName) throws IllegalArgumentException {
+        super(uniqueUsername, firstName, lastName);
+    }
 
 }
--- BugTrap/BugTrap/src/bugtrap03/usersystem/Developer.java
+++ BugTrap/BugTrap/src/bugtrap03/usersystem/Developer.java
@@ -1,13 +1,39 @@
 package bugtrap03.usersystem;
 
+/**
+ * 
+ * @author Admin
+ * @version 0.1
+ */
 public class Developer extends Issuer {
 
-	public Developer(String uniqueUsername, String firstName, String middleName, String lastName) {
-		super(uniqueUsername, firstName, middleName, lastName);
-	}
+    /**
+     * Create a {@link Developer} with a specific username, first name, middle name and last
+     * name.
+     *
+     * @param uniqueUsername The username
+     * @param firstName The first name
+     * @param middleName The middle name
+     * @param lastName The last name
+     * @throws IllegalArgumentException When the creator of Issuer throws it.
+     * @see Issuer#Issuer(java.lang.String, java.lang.String, java.lang.String, java.lang.String)
+     */
+    public Developer(String uniqueUsername, String firstName, String middleName, String lastName) throws IllegalArgumentException {
+        super(uniqueUsername, firstName, middleName, lastName);
+    }
 
-	public Developer(String uniqueUsername, String firstName, String lastName) {
-		super(uniqueUsername, firstName, lastName);
-	}
+    /**
+     * Create a {@link Developer} with a specific username, first name and last
+     * name.
+     *
+     * @param uniqueUsername The username
+     * @param firstName The first name
+     * @param lastName The last name
+     * @throws IllegalArgumentException When the creator of Issuer throws it.
+     * @see Issuer#Issuer(java.lang.String, java.lang.String, java.lang.String)
+     */
+    public Developer(String uniqueUsername, String firstName, String lastName) throws IllegalArgumentException {
+        super(uniqueUsername, firstName, lastName);
+    }
 
 }
