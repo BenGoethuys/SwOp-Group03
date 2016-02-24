7c0a323d881c345d7940c591722a8145a9d24358
Mathias Dekempeneer <mathiasdekempeneer@gmail.com>
2016-02-24 17:16:48 +0100

    projectParticipants

--- BugTrap/BugTrap/src/bugtrap03/bugdomain/Project.java
+++ BugTrap/BugTrap/src/bugtrap03/bugdomain/Project.java
@@ -1,31 +1,30 @@
 package bugtrap03.bugdomain;
 
 import java.util.Date;
-<<<<<<< HEAD
+import java.util.HashMap;
 
+import com.sun.xml.internal.messaging.saaj.packaging.mime.internet.ParameterList;
+
+import bugtrap03.usersystem.Administrator;
 import bugtrap03.usersystem.Developer;
 import bugtrap03.usersystem.Role;
+import bugtrap03.usersystem.User;
 import purecollections.PList;
-import purecollections.PMap;
 
-=======
 /**
- * This class extends AbstractSystem (versionID, name and description) and extends it with dates.
- * @author Kwinten
+ * This class extends AbstractSystem (versionID, name and description) and
+ * extends it with dates.
+ * 
+ * @Invariant
+ * @author
  *
  */
->>>>>>> fb1167a0928146628d1303528203fda4f1f5f014
 public class Project extends AbstractSystem {
 
 	private Date creationDate;
 	private Date startDate;
-<<<<<<< HEAD
-	private PMap<Developer, PList<Role>> projectParticipants;
-
-	// hoi iedereen, nu werk ik met eclipse
-=======
+	private HashMap<Developer, PList<Role>> projectParticipants;
 	private long budgetEstimate;
->>>>>>> fb1167a0928146628d1303528203fda4f1f5f014
 
 	/**
 	 * Creates a project with a given versionID, name, description, creationDate
@@ -40,15 +39,15 @@ public class Project extends AbstractSystem {
 	 * @throws IllegalArgumentException if one of the String arguments or dates
 	 *             is invalid.
 	 */
-	public Project(VersionID version, String name, String description, Date creationDate, Date startDate, long budgetEstimate)
-			throws IllegalArgumentException, NullPointerException {
+	public Project(VersionID version, String name, String description, Date creationDate, Date startDate,
+			long budgetEstimate) throws IllegalArgumentException, NullPointerException {
 		super(version, name, description);
-		if (! isValidStartDate(creationDate, startDate)) {
+		if (!isValidStartDate(creationDate, startDate)) {
 			throw new IllegalArgumentException("The project hasn't a valid creation and start date");
-		} 
+		}
 		setCreationDate(creationDate);
 		setStartDate(startDate);
-		setBudgetEstimate(budgetEstimate);	
+		setBudgetEstimate(budgetEstimate);
 	}
 
 	/**
@@ -114,40 +113,39 @@ public class Project extends AbstractSystem {
 			throw new NullPointerException("A date can't be null.");
 		}
 	}
-	
+
 	/**
 	 * 
 	 * @param budgetEstimate
 	 * @throws IllegalArgumentException
 	 */
-	private void setBudgetEstimate(long budgetEstimate) throws IllegalArgumentException{
-		if (! isValidBudgetEstimate(budgetEstimate)){
+	private void setBudgetEstimate(long budgetEstimate) throws IllegalArgumentException {
+		if (!isValidBudgetEstimate(budgetEstimate)) {
 			throw new IllegalArgumentException("invalid budgetestimate");
 		}
 		this.budgetEstimate = budgetEstimate;
-	}	
-	
+	}
+
 	/**
 	 * 
 	 * @param budgetEstimate
 	 * @return
 	 */
-	private boolean isValidBudgetEstimate(long budgetEstimate){
-		if (budgetEstimate < 0){
+	private boolean isValidBudgetEstimate(long budgetEstimate) {
+		if (budgetEstimate < 0) {
 			return false;
 		}
 		return true;
 	}
-	
+
 	/**
 	 * 
 	 * @return
 	 */
-	protected long getBudgetEstimate(){
+	protected long getBudgetEstimate() {
 		return this.budgetEstimate;
 	}
-		
-		
+
 	/**
 	 * This is a getter for the parent. Since a Project doesn't have a parent,
 	 * it returns itself.
@@ -157,6 +155,40 @@ public class Project extends AbstractSystem {
 	protected Project getParent() {
 		return this;
 	}
-	
-	
+
+	private void setParticipants(Developer dev, Role role) {
+		if (projectParticipants == null) {
+			throw new NullPointerException("Project Participants Map must not be null.");
+		}
+		PList<Role> roleList = projectParticipants.get(dev);
+		if (roleList == null) {
+			projectParticipants.put(dev, PList.<Role> empty().plus(role));
+		} else {
+
+		}
+	}
+
+	public PList<Role> getAllRolesDev(Developer dev) {
+		return projectParticipants.get(dev);
+	}
+
+	public boolean hasPermissionToSet(User user, Developer dev, Role role) {
+		if (user instanceof Administrator) {
+			if (role.equals(Role.LEAD)) {
+				return projectParticipants.values().parallelStream().anyMatch(k -> k.contains(role));
+			} else {
+				return true;
+			}
+		}
+		if (user instanceof Developer) {
+			if (role.equals(Role.LEAD)) {
+				return false;
+			} else {
+
+			}
+		}
+
+		return false;
+	}
+
 }
