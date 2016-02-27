458c010c640501ce471d13935b5c37efc8b7930e
VincentDerk <vincent.derkinderen@student.kuleuven.be>
2016-02-27 23:32:54 +0100

    WIP

--- BugTrap/BugTrap/src/bugtrap03/Main.java
+++ BugTrap/BugTrap/src/bugtrap03/Main.java
@@ -1,10 +1,10 @@
 package bugtrap03;
 
 import bugtrap03.gui.terminal.Terminal;
-import bugtrap03.usersystem.Administrator;
+import java.util.Scanner;
 
 /**
- * 
+ *
  * @author Group 03
  */
 public class Main {
@@ -14,22 +14,26 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-        DataModel model = new DataModel();
-        DataController controller = new DataController(model);
-        
+        model = new DataModel();
+        controller = new DataController(model);
+
         initDemoSystem(controller);
-        
+
         Terminal terminal = new Terminal(controller);
         terminal.openView();
     }
-    
+
+    private static DataModel model;
+    private static DataController controller;
+
     public static void initDemoSystem(DataController con) {
-        
-        
+        model.createAdministrator("curt", "Frederick", "Sam", "Curtis");
+        model.createIssuer("doc", "John", "Doctor");
+        model.createIssuer("charlie", "Charles", "Arnold", "Berg");
+        model.createDeveloper("major", "Joseph", "Mays");
+        model.createDeveloper("maria", "Maria", "Carney");
         
         
     }
 
-            
-            
 }
--- BugTrap/BugTrap/src/bugtrap03/gui/terminal/Terminal.java
+++ BugTrap/BugTrap/src/bugtrap03/gui/terminal/Terminal.java
@@ -1,6 +1,8 @@
 package bugtrap03.gui.terminal;
 
 import bugtrap03.DataController;
+import bugtrap03.usersystem.User;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -8,27 +10,85 @@ import java.util.Scanner;
  * @author Admin
  */
 public class Terminal {
-    
-    
-    public Terminal(DataController con) {
+
+    public Terminal(DataController con) throws IllegalArgumentException {
+        if (con == null) {
+            throw new IllegalArgumentException("Terminal does not allow a null-reference for DataController.");
+        }
         this.con = con;
-        this.parser = new CmdParser();
+        this.scan = new Scanner(System.in);
+        this.parser = new CmdParser(this);
     }
-    
+
     private DataController con;
     private CmdParser parser;
-    
-    public void openView() {
-        
-        //Login
-        //TODO: Finish parser.getLoginCmd()
-        
-        Scanner scan = new Scanner(System.in);
-        System.out.println(scan.hasNextInt());
-        System.out.println(scan.hasNextLine());
-        
-        
-        
+    private final Scanner scan;
+    private User user;
+
+    /**
+     * Get the {@link User} currently logged in as.
+     *
+     * @return The user currently logged in as.
+     */
+    public User getUser() {
+        return this.user;
     }
-    
+
+    /**
+     *
+     * @param user
+     */
+    public void setUser(User user) throws IllegalArgumentException {
+        if (user == null) {
+            throw new IllegalArgumentException("No null-reference allowed by the Terminal for User.");
+        }
+        this.user = user;
+    }
+
+    /**
+     * Get the {@link DataController} currently in use.
+     *
+     * @return The currently used controller.
+     */
+    public DataController getController() {
+        return this.con;
+    }
+
+    /**
+     * Get the {@link Scanner} currently in use.
+     *
+     * @return The currently used scanner.
+     */
+    public Scanner getScanner() {
+        return this.scan;
+    }
+
+    public void openView() {
+        //Login
+        new LoginCmd(this).exec(scan, con, null);
+        System.out.println("");
+
+        //Query
+        String input;
+        while (true) {
+            input = scan.nextLine();
+            parser.performCmd(scan, con, user, input);
+        }
+    }
+
+    public final static void clearConsole() {
+        try {
+            final String os = System.getProperty("os.name");
+
+            if (os.contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else { //ANSI & Pray :P
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (final IOException | InterruptedException e) {
+            //  Handle any exceptions.
+        }
+    }
+
 }
