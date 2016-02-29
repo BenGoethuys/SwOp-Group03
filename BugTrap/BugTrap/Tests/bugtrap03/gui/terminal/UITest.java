70d11e3f92ff026e258f58f2c60d9bb7dfe87598
VincentDerk <vincent.derkinderen@student.kuleuven.be>
2016-03-01 00:59:53 +0100

    Trying to get textuitester to work.

---
+++ BugTrap/BugTrap/Tests/bugtrap03/gui/terminal/UITest.java
@@ -0,0 +1,26 @@
+package bugtrap03.gui.terminal;
+
+import java.io.IOException;
+import org.junit.Test;
+import textuitester.TextUITestScriptRunner;
+import textuitester.TextUITester;
+
+/**
+ *
+ * @author Admin
+ */
+public class UITest {
+
+    /*@Test
+    public void scriptTest() throws IOException {
+        TextUITestScriptRunner.runTestScript(UITest.class, "testScript1.txt");
+    }*/
+
+    @Test
+    public void test() {
+        TextUITester tester = new TextUITester("java -cp bin bugtrap03.Main");
+        tester.sendLine("0");
+        tester.expectExit(0);
+    }
+
+}
