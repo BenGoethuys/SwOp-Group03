c5e841eaf962d6893a4634e01d5560b9a253a172
BenGoethuys <ben.goethuys@gmail.com>
2016-02-29 17:29:38 +0100

    Added showProjectCmd to parseList

--- BugTrap/BugTrap/src/bugtrap03/gui/terminal/CmdParser.java
+++ BugTrap/BugTrap/src/bugtrap03/gui/terminal/CmdParser.java
@@ -35,10 +35,13 @@ public class CmdParser {
         cmdList.add(new SimpleEntry("login", new LoginCmd(this.terminal)));
         cmdList.add(new SimpleEntry("createproject", new CreateProjectCmd()));
         cmdList.add(new SimpleEntry("updateproject", new UpdateProjectCmd()));
+        cmdList.add(new SimpleEntry("showprojectdetails", new ShowProjectCmd()));
         
         //Custom abbreviations.
         cmdList.add(new SimpleEntry("createproj", new CreateProjectCmd()));
         cmdList.add(new SimpleEntry("updateproj", new UpdateProjectCmd()));
+        cmdList.add(new SimpleEntry("showprojdetails", new ShowProjectCmd()));
+        cmdList.add(new SimpleEntry("showprojdet", new ShowProjectCmd()));
 
         cmdMap = new HashMap<>();
         for (int i = 0; i < cmdList.size(); i++) {
