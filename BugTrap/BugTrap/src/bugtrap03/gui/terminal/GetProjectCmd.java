bc5b05c566032f28888962952fa95864f94d2dba
BenGoethuys <ben.goethuys@gmail.com>
2016-02-29 17:39:50 +0100

    Fixed getProjectCmd headers

--- BugTrap/BugTrap/src/bugtrap03/gui/terminal/GetProjectCmd.java
+++ BugTrap/BugTrap/src/bugtrap03/gui/terminal/GetProjectCmd.java
@@ -1,21 +1,16 @@
-/**
- *
- */
 package bugtrap03.gui.terminal;
 
 import java.util.NoSuchElementException;
 
 import bugtrap03.DataController;
 import bugtrap03.bugdomain.Project;
-import bugtrap03.permission.PermissionException;
 import bugtrap03.usersystem.User;
 import purecollections.PList;
 
 /**
- *  //TODO write heading
+ * //TODO write heading
  *
  * @author Ben Goethuys
- *
  */
 public class GetProjectCmd implements Cmd {
 
@@ -24,17 +19,19 @@ public class GetProjectCmd implements Cmd {
      * projects.
      *
      * @param scan The scanner used to interact with the person.
-     * @param controller The controller used for model access.
+     * @param con  The controller used for model access.
      * @param user The {@link User} who wants to executes this command.
      * @return null if there is no result specified.
      * @throws CancelException When the users wants to abort the current cmd
-     * @see
-     * bugtrap03.gui.terminal.Cmd#exec(bugtrap03.gui.terminal.TerminalScanner,
+     * @see bugtrap03.gui.terminal.Cmd#exec(bugtrap03.gui.terminal.TerminalScanner,
      * bugtrap03.DataController, bugtrap03.usersystem.User)
+     * <p>
+     * <br> 1. The system shows a list of existing projects.
+     * <br> 2. The administrator selects an existing project.
      */
     @Override
     public Project exec(TerminalScanner scan, DataController con, User user) throws CancelException {
-		// show all projects
+        // show all projects
         PList<Project> projectList = con.getProjectList();
         System.out.println("Available projects:");
         for (int i = 0; i < projectList.size(); i++) {
