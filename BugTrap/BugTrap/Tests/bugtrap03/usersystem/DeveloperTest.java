98220b46cc4d10440c673db4bc3c5be224adc415
Mathias Dekempeneer <mathiasdekempeneer@gmail.com>
2016-02-29 14:52:55 +0100

    Start of tests for Developer and Issuer

---
+++ BugTrap/BugTrap/Tests/bugtrap03/usersystem/DeveloperTest.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package bugtrap03.usersystem;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Mathias //TODO
+ *
+ */
+public class DeveloperTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link bugtrap03.usersystem.Developer#hasRolePermission(bugtrap03.permission.RolePerm, bugtrap03.bugdomain.Project)}.
+	 */
+	@Test
+	public void testHasRolePermission() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bugtrap03.usersystem.Developer#Developer(java.lang.String, java.lang.String, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testDeveloperStringStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bugtrap03.usersystem.Developer#Developer(java.lang.String, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testDeveloperStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bugtrap03.usersystem.Issuer#Issuer(java.lang.String, java.lang.String, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testIssuerStringStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bugtrap03.usersystem.Issuer#Issuer(java.lang.String, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testIssuerStringStringString() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ BugTrap/BugTrap/Tests/bugtrap03/usersystem/IssuerTest.java
@@ -0,0 +1,152 @@
+/**
+ * 
+ */
+package bugtrap03.usersystem;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Mathias //TODO
+ *
+ */
+public class IssuerTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link bugtrap03.usersystem.Issuer#Issuer(java.lang.String, java.lang.String, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testIssuerStringStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bugtrap03.usersystem.Issuer#Issuer(java.lang.String, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testIssuerStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bugtrap03.usersystem.User#User(java.lang.String, java.lang.String, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testUserStringStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bugtrap03.usersystem.User#User(java.lang.String, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testUserStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bugtrap03.usersystem.User#getUsername()}.
+	 */
+	@Test
+	public void testGetUsername() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bugtrap03.usersystem.User#isValidUsername(java.lang.String)}.
+	 */
+	@Test
+	public void testIsValidUsername() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bugtrap03.usersystem.User#getFirstName()}.
+	 */
+	@Test
+	public void testGetFirstName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bugtrap03.usersystem.User#isValidFirstName(java.lang.String)}.
+	 */
+	@Test
+	public void testIsValidFirstName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bugtrap03.usersystem.User#getMiddleName()}.
+	 */
+	@Test
+	public void testGetMiddleName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bugtrap03.usersystem.User#isValidMiddleName(java.lang.String)}.
+	 */
+	@Test
+	public void testIsValidMiddleName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bugtrap03.usersystem.User#getLastName()}.
+	 */
+	@Test
+	public void testGetLastName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bugtrap03.usersystem.User#isValidLastName(java.lang.String)}.
+	 */
+	@Test
+	public void testIsValidLastName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bugtrap03.usersystem.User#getFullName()}.
+	 */
+	@Test
+	public void testGetFullName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bugtrap03.usersystem.User#hasPermission(bugtrap03.permission.UserPerm)}.
+	 */
+	@Test
+	public void testHasPermission() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bugtrap03.usersystem.User#hasRolePermission(bugtrap03.permission.RolePerm, bugtrap03.bugdomain.Project)}.
+	 */
+	@Test
+	public void testHasRolePermission() {
+		fail("Not yet implemented");
+	}
+
+}
