4f8eacb633ffd8db4ee2a4caec59638f12881929
Mathias Dekempeneer <mathiasdekempeneer@gmail.com>
2016-03-02 14:03:36 +0100

    Removed TODO

--- BugTrap/BugTrap/src/bugtrap03/gui/terminal/DeleteProjectCmd.java
+++ BugTrap/BugTrap/src/bugtrap03/gui/terminal/DeleteProjectCmd.java
@@ -19,29 +19,28 @@ public class DeleteProjectCmd implements Cmd {
      * 1. The administrator indicates he wants to delete a project. <br>
      * 2. The system shows a list of all projects. <br>
      * 3. The administrator selects a project. <br>
-     * 4. The system deletes a project and recursively all subsystems that are //TODO??
+     * 4. The system deletes a project and recursively all subsystems that are
      * part of the project. All bug reports fore those subsystem are also
      * removed from BugTrap.
      *
-     * @param scan  The {@link Scanner} trough which to ask the questions.
+     * @param scan The {@link Scanner} trough which to ask the questions.
      * @param model The model to use to access the model.
-     * @param user  The user who wants to execute this {@link Cmd}.
+     * @param user The user who wants to execute this {@link Cmd}.
      * @return The deleted project
      * @throws PermissionException When the user does not have sufficient
-     *                             permissions to update a project.
+     *             permissions to update a project.
      */
     @Override
-    public Project exec(TerminalScanner scan, DataModel model, User user)
+    public Project exec(TerminalScanner scan, DataModel model, User user) 
             throws PermissionException, CancelException {
 
         // Get project
         Project proj = new GetProjectCmd().exec(scan, model, user);
 
-        // Delete the project 
+        // Delete the project
         model.deleteProject(user, proj);
 
         System.out.println("Project deleted.");
         return proj;
     }
-
 }
