56d17519f2aac6558e81c5b4e68bf07e42a355da
BenGoethuys <ben.goethuys@gmail.com>
2016-03-04 19:25:13 +0100

    Added test for new method in Subsystem -> 100% coverage now

--- BugTrap/BugTrap/Tests/bugtrap03/bugdomain/SubsystemTest.java
+++ BugTrap/BugTrap/Tests/bugtrap03/bugdomain/SubsystemTest.java
@@ -23,36 +23,36 @@ public class SubsystemTest {
     static GregorianCalendar testCreationDate;
     static long testBudget;
     static Project testProject;
-    
+
     static VersionID subVersion;
     static String subName;
     static String subDescription;
     static Subsystem subSysTest;
     static Subsystem subSysTest2;
-    
+
     static PList<BugReport> emptyDep;
     static PList<BugReport> depToRep1;
     static BugReport bugreport1;
     static BugReport bugreport2;
-    
+
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
         testDev = new Developer("subsysTester321", "Kwinten", "Buytaert");
-        testVersion = new VersionID(1,2,4);
+        testVersion = new VersionID(1, 2, 4);
         testName = "testProj";
         testDescription = "This is an description";
         testStartDate = new GregorianCalendar(3016, 1, 1);
-        testCreationDate = new GregorianCalendar(2000,12,25);   
+        testCreationDate = new GregorianCalendar(2000, 12, 25);
         testBudget = 1000;
         testProject = new Project(testVersion, testName, testDescription, testCreationDate, testDev, testStartDate, testBudget);
-        
-        subVersion = new VersionID(9,8,7);
+
+        subVersion = new VersionID(9, 8, 7);
         subName = "testSub";
         subDescription = "This is a test description of a subsystem";
         subSysTest = testProject.makeSubsystemChild(subVersion, subName, subDescription);
         subSysTest2 = subSysTest.makeSubsystemChild("meh", "moeh");
-        
+
         emptyDep = PList.<BugReport>empty();
         bugreport1 = subSysTest.addBugReport(testDev, "testBug3", "this is description of testbug 3", emptyDep);
 
@@ -80,7 +80,7 @@ public class SubsystemTest {
         PList<BugReport> getRep2 = subSysTest2.getAllBugReports();
         assertEquals(expectedRep, getRep1);
         assertEquals(emptyDep, getRep2);
-        
+
     }
 
     @Test
@@ -98,10 +98,10 @@ public class SubsystemTest {
         assertEquals(testDescription, tempSub.getDescription());
         assertEquals(testProject, tempSub.getParent());
         assertEquals(emptyDep, tempSub.getBugReportList());
-        
+
     }
-    
-    @Test (expected = IllegalArgumentException.class)
+
+    @Test(expected = IllegalArgumentException.class)
     public void testSubsystemVersionIDStringStringNullAbstractSystem() {
         @SuppressWarnings("unused")
         Subsystem tempSub = new Subsystem(testVersion, subName, testDescription, null);
@@ -131,37 +131,41 @@ public class SubsystemTest {
         assertEquals(emptyDep, subSysTest2.getBugReportList());
     }
 
-    
-    
+
     @Test
     public void testAddBugReport() throws IllegalArgumentException, PermissionException {
-       BugReport bugreport3 = subSysTest.addBugReport(testDev, "otherBug5", "i have a love/hate relation with testing", emptyDep);
-       PList<BugReport> expectedRep1 = PList.<BugReport>empty().plus(bugreport1).plus(bugreport2);
-       assertNotEquals(expectedRep1, subSysTest.getBugReportList());
-       PList<BugReport> expectedRep2 = expectedRep1.plus(bugreport3);
-       assertEquals(expectedRep2, subSysTest.getBugReportList());
+        BugReport bugreport3 = subSysTest.addBugReport(testDev, "otherBug5", "i have a love/hate relation with testing", emptyDep);
+        PList<BugReport> expectedRep1 = PList.<BugReport>empty().plus(bugreport1).plus(bugreport2);
+        assertNotEquals(expectedRep1, subSysTest.getBugReportList());
+        PList<BugReport> expectedRep2 = expectedRep1.plus(bugreport3);
+        assertEquals(expectedRep2, subSysTest.getBugReportList());
+
+        // alt method for adding BugReport
+        BugReport bugReport4 = subSysTest.addBugReport(testDev, "Best title ever", "Yes you did not write this", new GregorianCalendar(2016, 3, 4), emptyDep);
+        PList<BugReport> expectedRep3 = expectedRep2.plus(bugReport4);
+        assertEquals(expectedRep3, subSysTest.getBugReportList());
     }
-    
-    @Test (expected = PermissionException.class)
+
+    @Test(expected = PermissionException.class)
     public void testAddInvalidPermissionBugReport() throws IllegalArgumentException, PermissionException {
-       Administrator admin = new Administrator("uniqueAdminunique", "adje", "minnie");
-       @SuppressWarnings("unused")
-    BugReport bugreport3 = subSysTest.addBugReport(admin, "otherBug5", "i have a love/hate relation with testing", emptyDep);
+        Administrator admin = new Administrator("uniqueAdminunique", "adje", "minnie");
+        @SuppressWarnings("unused")
+        BugReport bugreport3 = subSysTest.addBugReport(admin, "otherBug5", "i have a love/hate relation with testing", emptyDep);
     }
-    
-    @Test (expected = IllegalArgumentException.class)
+
+    @Test(expected = IllegalArgumentException.class)
     public void testAddInvalidTitleBugReport() throws IllegalArgumentException, PermissionException {
-       @SuppressWarnings("unused")
-    BugReport bugreport3 = subSysTest.addBugReport(testDev, null, "i have a love/hate relation with testing", emptyDep);
+        @SuppressWarnings("unused")
+        BugReport bugreport3 = subSysTest.addBugReport(testDev, null, "i have a love/hate relation with testing", emptyDep);
     }
 
     @Test
     public void testCloneSubsystem() {
-        Project cloneProj = new Project("cloneProjTest", "this project is supposed to be a clone", testDev, 99);        
+        Project cloneProj = new Project("cloneProjTest", "this project is supposed to be a clone", testDev, 99);
         Subsystem cloneSub = subSysTest.cloneSubsystem(cloneProj);
         assertEquals(subSysTest.getVersionID(), cloneSub.getVersionID());
-        assertEquals(subSysTest.getName(),cloneSub.getName());
-        assertEquals(subSysTest.getDescription(),cloneSub.getDescription());
+        assertEquals(subSysTest.getName(), cloneSub.getName());
+        assertEquals(subSysTest.getDescription(), cloneSub.getDescription());
         assertNotEquals(subSysTest.getParent(), cloneSub.getParent());
         assertEquals(cloneProj, cloneSub.getParent());
     }
