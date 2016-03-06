89b7d0b0d471728031201c298cde23961cf9e8ae
VincentDerk <vincent.derkinderen@student.kuleuven.be>
2016-03-06 10:58:03 +0100

    Format.

--- BugTrap/BugTrap/src/bugtrap03/gui/cmd/general/GetIntCmd.java
+++ BugTrap/BugTrap/src/bugtrap03/gui/cmd/general/GetIntCmd.java
@@ -10,26 +10,26 @@ import bugtrap03.gui.terminal.TerminalScanner;
  * Created by Ben on 04/03/2016.
  */
 public class GetIntCmd implements Cmd {
+
     /**
      * Execute this command and possibly return a result.
      *
-     * @param scan  The scanner used to interact with the person.
+     * @param scan The scanner used to interact with the person.
      * @param model The model used for model access.
-     * @param user  The {@link User} who wants to executes this command.
+     * @param user The {@link User} who wants to executes this command.
      * @return null if there is no result specified.
      * @throws PermissionException When the user does not have sufficient
-     *                             permissions.
-     * @throws CancelException     When the users wants to abort the current cmd
+     * permissions.
+     * @throws CancelException When the users wants to abort the current cmd
      */
     @Override
     public Integer exec(TerminalScanner scan, DataModel model, User user) throws PermissionException, CancelException {
         scan.print("Give number: ");
         Integer integer = null;
         do {
-            if (scan.hasNextInt()){
+            if (scan.hasNextInt()) {
                 integer = scan.nextInt();
-            }
-            else {
+            } else {
                 scan.println("Invalid input, please enter an number");
             }
         } while (integer == null);
--- BugTrap/BugTrap/src/bugtrap03/gui/cmd/general/GetLongCmd.java
+++ BugTrap/BugTrap/src/bugtrap03/gui/cmd/general/GetLongCmd.java
@@ -10,16 +10,17 @@ import bugtrap03.gui.terminal.TerminalScanner;
  * Created by Ben on 04/03/2016.
  */
 public class GetLongCmd implements Cmd {
+
     /**
      * Execute this command and possibly return a result.
      *
-     * @param scan  The scanner used to interact with the person.
+     * @param scan The scanner used to interact with the person.
      * @param model The model used for model access.
-     * @param user  The {@link User} who wants to executes this command.
+     * @param user The {@link User} who wants to executes this command.
      * @return null if there is no result specified.
      * @throws PermissionException When the user does not have sufficient
-     *                             permissions.
-     * @throws CancelException     When the users wants to abort the current cmd
+     * permissions.
+     * @throws CancelException When the users wants to abort the current cmd
      */
     @Override
     public Long exec(TerminalScanner scan, DataModel model, User user) throws PermissionException, CancelException {
