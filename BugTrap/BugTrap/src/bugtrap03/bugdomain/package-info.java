65e031a6e5f69c7e5ee820f204ca11439f653734
BenGoethuys <ben.goethuys@gmail.com>
2016-03-07 00:50:26 +0100

    Small change for consistency

--- BugTrap/BugTrap/src/bugtrap03/bugdomain/package-info.java
+++ BugTrap/BugTrap/src/bugtrap03/bugdomain/package-info.java
@@ -2,5 +2,5 @@
  * Define API on this package and all subpackages
  * Created by Ben Goethuys on 06/03/2016.
  */
-@capsules.Capsule(exportKeyword=DomainAPI.class, friends={"bugtrap03.model"})
+@capsules.Capsule(exportKeyword=bugtrap03.bugdomain.DomainAPI.class, friends={"bugtrap03.model"})
 package bugtrap03.bugdomain;
