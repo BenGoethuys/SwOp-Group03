eb530d6307e5e186a9bfa4aaad6059b1e98cfa8b
Mathias Dekempeneer <mathiasdekempeneer@gmail.com>
2016-03-07 17:05:32 +0100

    Initialized InspectBugReport

--- BugTrap/BugTrap/Tests/bugtrap03/gui/cmd/InspectBugReportCmdTest.java
+++ BugTrap/BugTrap/Tests/bugtrap03/gui/cmd/InspectBugReportCmdTest.java
@@ -1,37 +1,19 @@
-/**
- * 
- */
 package bugtrap03.gui.cmd;
 
-import static org.junit.Assert.*;
-
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import bugtrap03.model.DataModel;
-
 /**
- * @author Mathias
+ * TODO
+ * 
+ * @author Group 03
  *
  */
 public class InspectBugReportCmdTest {
 
-
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
-        model = new DataModel();
-        lead = model.createDeveloper("theLeader", "the", "Leader");
-        admin = model.createAdministrator("theAdmin", "Ad", "Min");
-        proj0 = model.createProject("ProjectTest0", "Project for testing 0", lead, 500, admin);
-        proj1 = model.createProject("ProjectTest1", "Project for testing 1", lead, 1000, admin);
-    }
 
-    /**
-     * @throws java.lang.Exception
-     */
-    @Before
-    public void setUp() throws Exception {
     }
 
     /**
@@ -39,7 +21,7 @@ public class InspectBugReportCmdTest {
      */
     @Test
     public void testExec() {
-        fail("Not yet implemented");
+        
     }
 
 }
