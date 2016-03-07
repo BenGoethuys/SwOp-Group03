9260d9298afce7b603f883139b3080823eda76a3
Mathias Dekempeneer <mathiasdekempeneer@gmail.com>
2016-03-07 16:24:15 +0100

    Initialized tests

---
+++ BugTrap/BugTrap/Tests/bugtrap03/gui/cmd/AssignToBugReportCmdTest.java
@@ -0,0 +1,24 @@
+package bugtrap03.gui.cmd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AssignToBugReportCmdTest {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testExec() {
+        fail("Not yet implemented");
+    }
+
+}
