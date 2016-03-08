501b6bfcb7f2d3d3eb55735f9a3fb602f1517dc1
VincentDerk <vincent.derkinderen@student.kuleuven.be>
2016-03-08 22:11:43 +0100

    Added few test. Openview is still left untested.

---
+++ BugTrap/BugTrap/Tests/bugtrap03/gui/terminal/TerminalTest.java
@@ -0,0 +1,45 @@
+package bugtrap03.gui.terminal;
+
+import bugtrap03.bugdomain.usersystem.User;
+import bugtrap03.model.DataModel;
+import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import org.junit.Test;
+
+/**
+ *
+ * @author Admin
+ */
+public class TerminalTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConsNullModel() {
+        Terminal term = new Terminal(null);
+    }
+
+    @Test
+    public void testCons() {
+        DataModel model = new DataModel();
+        Terminal term = new Terminal(model);
+        assertNotEquals(term.getModel(), null);
+        assertNotEquals(term.getScanner(), null);
+        assertEquals(term.getUser(), null);
+    }
+
+    @Test
+    public void testSetUser() {
+        DataModel model = new DataModel();
+        Terminal term = new Terminal(model);
+        User user = model.createAdministrator("Ping", "Pong", "Protocol");
+        term.setUser(user);
+        assertEquals(term.getUser(), user);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetUserNull() {
+        DataModel model = new DataModel();
+        Terminal term = new Terminal(model);
+        term.setUser(null);
+    }
+}
