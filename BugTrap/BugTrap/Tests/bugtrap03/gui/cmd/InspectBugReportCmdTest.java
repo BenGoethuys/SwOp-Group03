642b8390a8ff0273663573cd462f2f6e375491fb
Mathias Dekempeneer <mathiasdekempeneer@gmail.com>
2016-03-09 14:14:43 +0100

    Start tests cmd inspectBugReport

--- BugTrap/BugTrap/Tests/bugtrap03/gui/cmd/InspectBugReportCmdTest.java
+++ BugTrap/BugTrap/Tests/bugtrap03/gui/cmd/InspectBugReportCmdTest.java
@@ -1,8 +1,20 @@
 package bugtrap03.gui.cmd;
 
-import static org.junit.Assert.fail;
-import org.junit.BeforeClass;
+
+import java.util.ArrayDeque;
+
 import org.junit.Test;
+
+import bugtrap03.bugdomain.BugReport;
+import bugtrap03.bugdomain.Project;
+import bugtrap03.bugdomain.Subsystem;
+import bugtrap03.bugdomain.VersionID;
+import bugtrap03.bugdomain.permission.PermissionException;
+import bugtrap03.bugdomain.usersystem.Administrator;
+import bugtrap03.bugdomain.usersystem.Developer;
+import bugtrap03.bugdomain.usersystem.Issuer;
+import bugtrap03.model.DataModel;
+import purecollections.PList;
 
 /**
  * 
@@ -11,18 +23,47 @@ import org.junit.Test;
  */
 public class InspectBugReportCmdTest {
 
-    @BeforeClass
-    public static void setUpBeforeClass() throws Exception {
-
-    }
+    DataModel model;
+    Developer lead;
+    Issuer issuer;
+    Administrator admin;
+    Project projectA;
+    Subsystem subsystemA2;
+    Subsystem subsystemA3;
+    Subsystem subsystemA3_1;
+    BugReport bugRep1;
+    BugReport bugRep2;
 
     /**
-     * Test method for {@link bugtrap03.gui.cmd.InspectBugReportCmd#exec(bugtrap03.gui.terminal.TerminalScanner, bugtrap03.model.DataModel, bugtrap03.bugdomain.usersystem.User)}.
+     * Test method for
+     * {@link bugtrap03.gui.cmd.InspectBugReportCmd#exec(bugtrap03.gui.terminal.TerminalScanner, bugtrap03.model.DataModel, bugtrap03.bugdomain.usersystem.User)}
+     * .
      */
     @Test
-    public void testExec() {
-        //TODO: Implement InspectBugReportCmdTest
-        fail("Not implemented yet.");
+    public void testExec() throws IllegalArgumentException, PermissionException {
+        // Setup variables.
+        model = new DataModel();
+        lead = model.createDeveloper("Leader007", "Leader007", "Leader007");
+        issuer = model.createIssuer("Issuer007", "Issuer007", "Issuer007", "Issuer007");
+        admin = model.createAdministrator("Admin007", "Admin007", "Admin007");
+
+        projectA = model.createProject("ProjectTest0", "Project for testing 0", lead, 500, admin);
+        // make subsystems
+        subsystemA2 = model.createSubsystem(admin, projectA, new VersionID(), "SubsystemA2",
+                "Description of susbsystem A2");
+        subsystemA3 = model.createSubsystem(admin, projectA, new VersionID(), "SubsystemA3",
+                "Description of susbsystem A3");
+        subsystemA3_1 = model.createSubsystem(admin, subsystemA3, new VersionID(), "SubsystemA3.1",
+                "Description of susbsystem A3.1");
+
+        bugRep1 = model.createBugReport(issuer, "bugRep over here", "createComment has an output error",
+                PList.<BugReport> empty(), subsystemA2);
+        bugRep2 = model.createBugReport(issuer, "Used library not in repository", "title says it all.",
+                PList.<BugReport> empty(), subsystemA3_1);
+
+        ArrayDeque<String> question = new ArrayDeque<>();
+        ArrayDeque<String> answer = new ArrayDeque<>();
+        UpdateBugReportCmd cmd = new UpdateBugReportCmd();
     }
 
 }
