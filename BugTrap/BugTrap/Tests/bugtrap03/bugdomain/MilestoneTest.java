7feb5dfe213749a19b1e6e0b345141d85640b386
Mathias Dekempeneer <mathiasdekempeneer@gmail.com>
2016-03-16 15:23:20 +0100

    Added Milestone + testcoverage Milestone = 100%

--- BugTrap/BugTrap/Tests/bugtrap03/bugdomain/MilestoneTest.java
+++ BugTrap/BugTrap/Tests/bugtrap03/bugdomain/MilestoneTest.java
@@ -1,12 +1,8 @@
-/**
- * 
- */
 package bugtrap03.bugdomain;
 
 import static org.junit.Assert.*;
 
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -15,18 +11,24 @@ import org.junit.Test;
  */
 public class MilestoneTest {
 
-    /**
-     * @throws java.lang.Exception
-     */
-    @BeforeClass
-    public static void setUpBeforeClass() throws Exception {
-    }
+    private Milestone milestone1;
+    private Milestone milestone2;
+    private Milestone milestone3;
+    private String string1;
+    private String string2;
+    private String string3;
 
     /**
      * @throws java.lang.Exception
      */
     @Before
     public void setUp() throws Exception {
+        milestone1 = new Milestone(1);
+        milestone2 = new Milestone(1, 2);
+        milestone3 = new Milestone(1, 2, 3);
+        string1 = "M1";
+        string2 = "M1.2";
+        string3 = "M1.2.3";
     }
 
     /**
@@ -34,15 +36,17 @@ public class MilestoneTest {
      */
     @Test
     public void testToString() {
-        fail("Not yet implemented");
-    }
+        assertEquals(string1, milestone1.toString());
+        assertNotEquals("M1.0", milestone1.toString());
+        assertNotEquals("M1.0.0", milestone1.toString());
 
-    /**
-     * Test method for {@link bugtrap03.bugdomain.Milestone#Milestone(int, int, int)}.
-     */
-    @Test
-    public void testMilestone() {
-        fail("Not yet implemented");
+        assertEquals(string2, milestone2.toString());
+        assertNotEquals("M1", milestone2.toString());
+        assertNotEquals("M1.2.0", milestone2.toString());
+
+        assertEquals(string3, milestone3.toString());
+        assertNotEquals("M1", milestone3.toString());
+        assertNotEquals("M1.2.0", milestone3.toString());
     }
 
 }
--- BugTrap/BugTrap/src/bugtrap03/bugdomain/Milestone.java
+++ BugTrap/BugTrap/src/bugtrap03/bugdomain/Milestone.java
@@ -4,36 +4,53 @@ package bugtrap03.bugdomain;
  * @author Mathias
  *
  */
+@DomainAPI
 public class Milestone extends VersionID {
 
     /**
+     * Creates a Milestone.
      * 
-     * @param first
-     * @param second
-     * @param third
+     * @param first The first digit of the Milestone.
+     * @param second The second digit of the Milestone.
+     * @param third The third digit of the Milestone.
+     * 
+     * @see VersionID#VersionID(int, int, int)
      */
+    @DomainAPI
     public Milestone(int first, int second, int third) {
         super(first, second, third);
     }
 
     /**
+     * Creates a Milestone
      * 
-     * @param first
-     * @param second
+     * @param first The first digit of the Milestone.
+     * @param second The second digit of the Milestone.
+     * 
+     * @see VersionID#VersionID(int, int)
      */
+    @DomainAPI
     public Milestone(int first, int second) {
         super(first, second);
     }
 
     /**
+     * Creates a Milestone
      * 
-     * @param first
+     * @param first The first digit of the Milestone.
+     * 
+     * @see VersionID#VersionID(int)
      */
+    @DomainAPI
     public Milestone(int first) {
         super(first);
     }
 
+    /**
+     * Visual representation of a Milestone.
+     */
     @Override
+    @DomainAPI
     public String toString() {
         if (this.getThirdNb() != 0) {
             return "M" + this.getFirstNb() + "." + this.getSecondNb() + "." + this.getThirdNb();
