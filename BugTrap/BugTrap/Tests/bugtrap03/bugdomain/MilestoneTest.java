b566f50a3a9595d427c0b98c33c4e06e3e3e784c
Mathias Dekempeneer <mathiasdekempeneer@gmail.com>
2016-03-16 15:01:31 +0100

    Initialize tests

---
+++ BugTrap/BugTrap/Tests/bugtrap03/bugdomain/MilestoneTest.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package bugtrap03.bugdomain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Mathias
+ *
+ */
+public class MilestoneTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * Test method for {@link bugtrap03.bugdomain.Milestone#toString()}.
+     */
+    @Test
+    public void testToString() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link bugtrap03.bugdomain.Milestone#Milestone(int, int, int)}.
+     */
+    @Test
+    public void testMilestone() {
+        fail("Not yet implemented");
+    }
+
+}
