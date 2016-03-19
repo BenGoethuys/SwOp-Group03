308365fcb7002710dab03850dcb2c74c5b4adaea
BenGoethuys <ben.goethuys@gmail.com>
2016-03-19 16:27:57 +0100

    Removed old isValid method + removed old needed RolePerms that not exist anymore

--- BugTrap/BugTrap/src/bugtrap03/bugdomain/Tag.java
+++ BugTrap/BugTrap/src/bugtrap03/bugdomain/Tag.java
@@ -12,9 +12,9 @@ public enum Tag {
     NEW(RolePerm.SPECIAL),
     ASSIGNED(RolePerm.SPECIAL),
     NOT_A_BUG(RolePerm.SET_TAG_NOT_A_BUG),
-    UNDER_REVIEW(RolePerm.SET_TAG_UNDER_REVIEW),
-    CLOSED(RolePerm.SET_TAG_CLOSED),
-    RESOLVED(RolePerm.SET_TAG_RESOLVED),
+    UNDER_REVIEW(RolePerm.SPECIAL),
+    CLOSED(RolePerm.SPECIAL),
+    RESOLVED(RolePerm.SPECIAL),
     DUPLICATE(RolePerm.SET_TAG_DUPLICATE);
 
     /**
@@ -37,40 +37,5 @@ public enum Tag {
     @DomainAPI
     public RolePerm getNeededPerm() {
         return this.neededPerm;
-    }
-
-    /**
-     * This method check if the given tag is valid for the bug report
-     *
-     * @param tag the tag to check
-     * @return true if the tag is a valid tag
-     */
-    //TODO: We could move this logic into a variable of each enum, like with the roles/perms?
-    @DomainAPI
-    public boolean isValidTag(Tag tag) {
-        if (tag == null) {
-            return false;
-        }
-        // Closed, Duplicate, NotABug is unchangeable
-        if (this == Tag.CLOSED || this == Tag.DUPLICATE || this == Tag.NOT_A_BUG) {
-            return false;
-        }
-        // will be null in initialization: only moment Tag.New can be assigned
-        if (tag == Tag.NEW) {
-            return false;
-        }
-        if (tag == Tag.ASSIGNED && (this != Tag.NEW && this != Tag.UNDER_REVIEW)) {
-            return false;
-        }
-        if (tag == Tag.UNDER_REVIEW && this != Tag.ASSIGNED) {
-            return false;
-        }
-        if (tag == Tag.RESOLVED && this != Tag.UNDER_REVIEW) {
-            return false;
-        }
-        if (tag == Tag.CLOSED && (this != Tag.RESOLVED && this != Tag.UNDER_REVIEW)) {
-            return false;
-        }
-        return true;
     }
 }
