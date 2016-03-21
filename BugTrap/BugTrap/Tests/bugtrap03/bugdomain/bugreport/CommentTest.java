575b1bba1296128897272a6065fdac62cc25f250
VincentDerk <vincent.derkinderen@student.kuleuven.be>
2016-03-21 23:56:57 +0100

    Delete unnecessary import.

--- BugTrap/BugTrap/Tests/bugtrap03/bugdomain/bugreport/CommentTest.java
+++ BugTrap/BugTrap/Tests/bugtrap03/bugdomain/bugreport/CommentTest.java
@@ -3,7 +3,6 @@
  */
 package bugtrap03.bugdomain.bugreport;
 
-import bugtrap03.bugdomain.bugreport.Comment;
 import bugtrap03.bugdomain.permission.PermissionException;
 import bugtrap03.bugdomain.usersystem.Administrator;
 import bugtrap03.bugdomain.usersystem.Issuer;
