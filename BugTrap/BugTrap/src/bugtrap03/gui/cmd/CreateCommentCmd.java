4ae9a48ef48c776491044a3da575ae68eae1750d
BenGoethuys <ben.goethuys@gmail.com>
2016-03-21 17:34:35 +0100

    Moved Comment to bug report package

--- BugTrap/BugTrap/src/bugtrap03/gui/cmd/CreateCommentCmd.java
+++ BugTrap/BugTrap/src/bugtrap03/gui/cmd/CreateCommentCmd.java
@@ -1,7 +1,7 @@
 package bugtrap03.gui.cmd;
 
 import bugtrap03.bugdomain.bugreport.BugReport;
-import bugtrap03.bugdomain.Comment;
+import bugtrap03.bugdomain.bugreport.Comment;
 import bugtrap03.bugdomain.permission.PermissionException;
 import bugtrap03.bugdomain.usersystem.User;
 import bugtrap03.gui.cmd.general.CancelException;
