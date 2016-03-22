78d792694050dc1cd36839f14de74ea5f8860689
VincentDerk <vincent.derkinderen@student.kuleuven.be>
2016-03-22 23:55:30 +0100

    Moved order of methods.

--- BugTrap/BugTrap/src/bugtrap03/bugdomain/usersystem/mail/Subject.java
+++ BugTrap/BugTrap/src/bugtrap03/bugdomain/usersystem/mail/Subject.java
@@ -8,32 +8,32 @@ import java.util.Set;
  * @author Admin
  */
 public abstract class Subject {
-    
-    private Set<Observer> observers = new HashSet<>();
-    
+
     public void sub(Observer obs) throws IllegalArgumentException {
-        if(obs == null) {
+        if (obs == null) {
             throw new IllegalArgumentException("Can not subscribe a null reference as observer for this subject.");
         }
-        
+
         this.observers.add(obs);
     }
-    
+
+    private Set<Observer> observers = new HashSet<>();
+
     public boolean unsub(Observer obs) throws IllegalArgumentException {
-        if(obs == null) {
+        if (obs == null) {
             throw new IllegalArgumentException("Can not unsubscribe a null reference observer.");
         }
-        
+
         return this.observers.remove(obs);
     }
-    
+
     /**
      * Notify all our observers that the state has been updated.
      */
     public void notifyChanges() {
-        for(Observer obs : observers) {
+        for (Observer obs : observers) {
             obs.update(this);
         }
     }
-    
+
 }
