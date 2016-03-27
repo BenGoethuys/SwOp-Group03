9d5688754bc64a001c35d585aadd18ddc10e6f6a
VincentDerk <vincent.derkinderen@student.kuleuven.be>
2016-03-27 22:28:30 +0200

    Added delete bugReport and formatted.

--- BugTrap/BugTrap/src/bugtrap03/bugdomain/Subsystem.java
+++ BugTrap/BugTrap/src/bugtrap03/bugdomain/Subsystem.java
@@ -9,9 +9,8 @@ import purecollections.PList;
 import java.util.GregorianCalendar;
 
 /**
- * This class is a subclass of abstract system (versionID, name, description and
- * Childs list). This type also contains a bug report PList, containing the bug
- * reports linked to this subsystem.
+ * This class is a subclass of abstract system (versionID, name, description and Childs list). This type also contains a
+ * bug report PList, containing the bug reports linked to this subsystem.
  *
  * @author Group 03
  */
@@ -19,21 +18,19 @@ import java.util.GregorianCalendar;
 public class Subsystem extends AbstractSystem {
 
     /**
-     * This constructor makes an element of the class subsystem, using it's
-     * superclass, AbstractSystem, constructor.
+     * This constructor makes an element of the class subsystem, using it's superclass, AbstractSystem, constructor.
      *
      * @param version The versionID (of that type) of this element.
      * @param name The string name for this element.
      * @param description The string description of this element.
      * @param parent The parent (a Project or Subsystem) of this element.
-     * @throws IllegalArgumentException if one of the String arguments is
-     *             invalid.
+     * @throws IllegalArgumentException if one of the String arguments is invalid.
      * @throws IllegalArgumentException if the version id is invalid.
-     * @throws IllegalArgumentException if the parent is invalid for this
-     *             subsystem
+     * @throws IllegalArgumentException if the parent is invalid for this subsystem
      * @see AbstractSystem#AbstractSystem(VersionID, String, String)
      * @see Subsystem#isValidParent(AbstractSystem)
-     **/
+     *
+     */
     public Subsystem(VersionID version, String name, String description, AbstractSystem parent)
             throws IllegalArgumentException {
         super(version, name, description);
@@ -42,20 +39,19 @@ public class Subsystem extends AbstractSystem {
         // given parent");
         // }
         this.setParent(parent);
-        this.bugReportList = PList.<BugReport> empty();
+        this.bugReportList = PList.<BugReport>empty();
     }
 
     /**
-     * This constructor makes an element of the class subsystem, using it's
-     * superclass, AbstractSystem, constructor.
+     * This constructor makes an element of the class subsystem, using it's superclass, AbstractSystem, constructor.
      *
      * @param name The string name for this element.
      * @param description The string description of this element.
      * @param parent The parent (a Project or Subsystem) of this element.
-     * @throws IllegalArgumentException if one of the String arguments is
-     *             invalid.
+     * @throws IllegalArgumentException if one of the String arguments is invalid.
      * @see Subsystem#Subsystem(VersionID, String, String, AbstractSystem)
-     **/
+     *
+     */
     public Subsystem(String name, String description, AbstractSystem parent) throws IllegalArgumentException {
         super(name, description);
         // if (!this.isValidName(name, parent)) {
@@ -63,19 +59,18 @@ public class Subsystem extends AbstractSystem {
         // given parent");
         // }
         this.setParent(parent);
-        this.bugReportList = PList.<BugReport> empty();
+        this.bugReportList = PList.<BugReport>empty();
     }
 
     private AbstractSystem parent;
     private PList<BugReport> bugReportList;
 
     /**
-     * Sets the parent of the AbstractSystem to the given parent, if valid. Only
-     * elements of subclass subsystems have a parent different from null.
+     * Sets the parent of the AbstractSystem to the given parent, if valid. Only elements of subclass subsystems have a
+     * parent different from null.
      *
      * @param parent The given parent of the AbstractSystem
-     * @throws IllegalArgumentException if the given parent isn't valid for this
-     *             subsystem
+     * @throws IllegalArgumentException if the given parent isn't valid for this subsystem
      * @see Subsystem#isValidParent(AbstractSystem)
      */
     private void setParent(AbstractSystem parent) throws IllegalArgumentException {
@@ -86,8 +81,7 @@ public class Subsystem extends AbstractSystem {
     }
 
     /**
-     * This function checks or the given parent isn't one of subsystem's own
-     * child
+     * This function checks or the given parent isn't one of subsystem's own child
      *
      * @param parent The given parent to be checked.
      * @return true is the given parent isn't the subsystem's own child
@@ -142,19 +136,18 @@ public class Subsystem extends AbstractSystem {
     }
 
     /**
-     * This method creates and adds a bug report to the list of associated
-     * bugReports of this subsystem
+     * This method creates and adds a bug report to the list of associated bugReports of this subsystem
      *
-     * @param creator      The User that wants to create this bug report
-     * @param title        The title of the bugReport
-     * @param description  The description of the bugReport
+     * @param creator The User that wants to create this bug report
+     * @param title The title of the bugReport
+     * @param description The description of the bugReport
      * @param creationDate The creationDate of the bugReport
      * @param dependencies The depended bug reports of this bug report
-     * @param milestone    The milestone of the bug report
-     * @param isPrivate    The boolean that says if this bug report should be private or not
-     * @param trigger      A trigger used to trigger the bug
-     * @param stacktrace   The stacktrace got when the bug was triggered
-     * @param error        The error got when the bug was triggered
+     * @param milestone The milestone of the bug report
+     * @param isPrivate The boolean that says if this bug report should be private or not
+     * @param trigger A trigger used to trigger the bug
+     * @param stacktrace The stacktrace got when the bug was triggered
+     * @param error The error got when the bug was triggered
      *
      * @throws IllegalArgumentException if isValidCreator(creator) fails
      * @throws IllegalArgumentException if isValidUniqueID(uniqueID) fails
@@ -164,11 +157,12 @@ public class Subsystem extends AbstractSystem {
      * @throws IllegalArgumentException if isValidDependencies(dependencies) fails
      * @throws IllegalArgumentException if isValidSubSystem(subsystem) fails
      * @throws IllegalArgumentException if isValidMilestone(milestone) fails
-     * @throws PermissionException      if the given creator doesn't have the needed permission to create a bug report
+     * @throws PermissionException if the given creator doesn't have the needed permission to create a bug report
      *
      * @return The create bug report
      *
-     * <br><dt><b>Postconditions:</b><dd> if creationDate == null: result.getDate() == current date at the moment of initialization
+     * <br><dt><b>Postconditions:</b><dd> if creationDate == null: result.getDate() == current date at the moment of
+     * initialization
      * <br><dt><b>Postconditions:</b><dd> result.getUniqueID() is an unique ID for this bug report
      *
      * @see BugReport#isValidCreator(User)
@@ -182,8 +176,8 @@ public class Subsystem extends AbstractSystem {
      */
     @Ensures("result.getTag() == Tag.New && result.getUniqueID() != null")
     public BugReport addBugReport(User creator, String title, String description, GregorianCalendar creationDate,
-                                  PList<BugReport> dependencies, Milestone milestone, boolean isPrivate,
-                                  String trigger, String stacktrace, String error)
+            PList<BugReport> dependencies, Milestone milestone, boolean isPrivate,
+            String trigger, String stacktrace, String error)
             throws IllegalArgumentException, PermissionException {
         BugReport bugReport;
         if (creationDate == null) {
@@ -198,14 +192,23 @@ public class Subsystem extends AbstractSystem {
     }
 
     /**
-     * Returns a copy of the current subsystem, using the same versionID, name,
-     * description and parent, but with a removal of the bug reports addressed
-     * to this subsystem.
+     * Remove the given bugReport from the list of bugReports.
+     * <br> <b>Only use with Caution</b>: No other changes will be made. (e.g no dependencies or milestones will be
+     * changed.)
+     *
+     * @param bugReport The bugReport to delete.
+     */
+    public void deleteBugReport(BugReport bugReport) {
+        this.bugReportList = this.bugReportList.minus(bugReport);
+    }
+
+    /**
+     * Returns a copy of the current subsystem, using the same versionID, name, description and parent, but with a
+     * removal of the bug reports addressed to this subsystem.
      *
      * @return the clone of the subsystem
      * @throws IllegalArgumentException if the VersionID is invalid
-     * @throws IllegalArgumentException if one of the string arguments is
-     *             invalid
+     * @throws IllegalArgumentException if one of the string arguments is invalid
      * @throws IllegalArgumentException if the parent is invalid
      * @see Subsystem#Subsystem(VersionID, String, String, AbstractSystem)
      */
@@ -218,10 +221,9 @@ public class Subsystem extends AbstractSystem {
     }
 
     //TODO constructor with additional info
-
     /**
-     * this method implements the abstract method from abstract system to return
-     * a string of details belonging to this subsystem.
+     * this method implements the abstract method from abstract system to return a string of details belonging to this
+     * subsystem.
      *
      * @return a string of details
      */
@@ -244,13 +246,11 @@ public class Subsystem extends AbstractSystem {
     }
 
     /**
-     * This function checks the validity of the given name, in combination with
-     * its parent.
+     * This function checks the validity of the given name, in combination with its parent.
      *
      * @param name The string argument to be used as name.
      * @param parent The parent of the element to be named.
-     * @return true if no of the other child of the projectParent has the same
-     *         name.
+     * @return true if no of the other child of the projectParent has the same name.
      */
     // protected boolean isValidName(String name, AbstractSystem parent) {
     // if (!this.isValidName(name)) {
@@ -262,12 +262,10 @@ public class Subsystem extends AbstractSystem {
     // }
     // return childNamesNotEqual(name, parentProject);
     // }
-
     /**
-     * This function checks of none of the other subsystems (childs) of the
-     * AbstracSystem (parent) have the same name. This is done recursively. To
-     * be correctly used, the function should be called the first time with the
-     * Project of the three.
+     * This function checks of none of the other subsystems (childs) of the AbstracSystem (parent) have the same name.
+     * This is done recursively. To be correctly used, the function should be called the first time with the Project of
+     * the three.
      *
      * @param name The name to check.
      * @param parent The given parent.
@@ -284,5 +282,4 @@ public class Subsystem extends AbstractSystem {
     // }
     // return true;
     // }
-
 }
