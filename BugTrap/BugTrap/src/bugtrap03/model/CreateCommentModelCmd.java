b94ef810863b8b394bec8b325e83a63a4d72dec7
VincentDerk <vincent.derkinderen@student.kuleuven.be>
2016-03-29 11:08:06 +0200

    Added start of another branch for CreateCommentModelCmd (creating a subComment)

--- BugTrap/BugTrap/src/bugtrap03/model/CreateCommentModelCmd.java
+++ BugTrap/BugTrap/src/bugtrap03/model/CreateCommentModelCmd.java
@@ -8,9 +8,21 @@ import bugtrap03.bugdomain.usersystem.User;
 
 /**
  *
- * @author Admin
+ * @author Group 03
  */
 class CreateCommentModelCmd extends ModelCmd {
+
+    //TODO Finish constructor for creating comment on comment.
+    CreateCommentModelCmd(User user, BugReport bugReport, Comment parentComment, String text) throws IllegalArgumentException {
+        if (bugReport == null) {
+            throw new IllegalArgumentException("The bugReport passed to the CreateCommentModelCmd was a null reference.");
+        }
+
+        this.user = user;
+        this.bugReport = bugReport;
+        this.parentComment = parentComment;
+        this.text = text;
+    }
 
     /**
      * Create a {@link ModelCmd} that can create a {@link Comment} when executed.
@@ -18,7 +30,7 @@ class CreateCommentModelCmd extends ModelCmd {
      * @param user The creator of the comment
      * @param bugReport The bug report to create the comment on
      * @param text The text of the new comment
-     * 
+     *
      * @throws IllegalArgumentException When bugReport == null
      */
     CreateCommentModelCmd(User user, BugReport bugReport, String text) throws IllegalArgumentException {
@@ -29,11 +41,14 @@ class CreateCommentModelCmd extends ModelCmd {
         this.user = user;
         this.bugReport = bugReport;
         this.text = text;
+        parentComment = null;
     }
 
     private final User user;
     private final BugReport bugReport;
     private final String text;
+
+    private final Comment parentComment;
 
     private boolean isExecuted;
 
@@ -41,7 +56,7 @@ class CreateCommentModelCmd extends ModelCmd {
 
     /**
      * This method creates a comment on a given BugReport
-     * 
+     *
      * @return The new generated comment
      * @throws PermissionException If the given User doesn't have the permission to create the comment
      * @throws IllegalStateException When this ModelCmd was already executed.
@@ -55,7 +70,11 @@ class CreateCommentModelCmd extends ModelCmd {
             throw new IllegalStateException("The CreateCommentModelCmd was already executed.");
         }
 
-        comment = bugReport.addComment(user, text);
+        if (parentComment == null) {
+            comment = bugReport.addComment(user, text);
+        } else {
+            throw new IllegalStateException("Not yet implemented!"); //TODO implement creating subComment.
+        }
         isExecuted = true;
         return comment;
     }
@@ -66,8 +85,8 @@ class CreateCommentModelCmd extends ModelCmd {
             return false;
         }
 
-        j 
-        return true;
+        //TODO: implement undo'ing subcomment.
+        return bugReport.deleteComment(comment);
     }
 
     @Override
