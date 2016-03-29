5f0909b1754495bde827595979695e2677b79b41
KwintenB <kwinten.buytaert@hotmail.com>
2016-03-29 15:47:43 +0200

    added notify functions

--- BugTrap/BugTrap/src/bugtrap03/bugdomain/usersystem/notification/Subject.java
+++ BugTrap/BugTrap/src/bugtrap03/bugdomain/usersystem/notification/Subject.java
@@ -1,5 +1,7 @@
 package bugtrap03.bugdomain.usersystem.notification;
 
+import bugtrap03.bugdomain.bugreport.BugReport;
+import bugtrap03.bugdomain.bugreport.Comment;
 import purecollections.PList;
 
 /**
@@ -8,6 +10,9 @@ import purecollections.PList;
  */
 public abstract class Subject {
 
+    /**
+     * The abstract constructor for a subject, sets the subs to empty Plists.
+     */
     public Subject(){
         this.tagSubs = PList.<TagMailBox>empty();
         this.commentSubs = PList.<CommentMailBox>empty();
@@ -16,5 +21,32 @@ public abstract class Subject {
     private PList<TagMailBox> tagSubs;
     private PList<CommentMailBox> commentSubs;
 
-    protected abstract String getSubjectName();
+    /**
+     * This abstract method returns a string representation of the subject name and type.
+     *
+     * @return the string containing subject name and type.
+     */
+    public abstract String getSubjectName();
+
+    /**
+     * This method updates all the mailboxes subscribed on a tag change from this subject.
+     *
+     * @param br The bugreport needed for the update
+     * @see TagMailBox#update(BugReport)
+     */
+    protected void updateTagSubs(BugReport br){
+        for (TagMailBox tmb: this.tagSubs){
+            tmb.update(br);
+        }
+    }
+
+    protected abstract void notifyTagSubs(BugReport br);
+
+    protected void updateCommentSubs(BugReport br){
+        for (CommentMailBox cmb: this.commentSubs){
+            cmb.update(br);
+        }
+    }
+
+    protected abstract void notifyCommentSubs(BugReport br);
 }
