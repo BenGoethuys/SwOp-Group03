4fdb6c6db1298dc39195669ceeba4bcaead77634
VincentDerk <vincent.derkinderen@student.kuleuven.be>
2016-04-03 11:53:18 +0200

    Added more tests. (Tested remove, clearLeftNull, removeTree, iterator, nodeIterator)

--- BugTrap/BugTrap/Tests/bugtrap03/misc/TreeTest.java
+++ BugTrap/BugTrap/Tests/bugtrap03/misc/TreeTest.java
@@ -1,11 +1,12 @@
 package bugtrap03.misc;
 
-import org.junit.Assert;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import org.junit.Before;
+import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import org.junit.Before;
-import org.junit.Test;
 
 /**
  *
@@ -13,22 +14,35 @@ import org.junit.Test;
  */
 public class TreeTest {
 
-    //TODO: Tests for addAll, remove, removeAll, retainAll, iterator, nodeIterator
-    
+    //TODO: Tests for addAll, removeAll, retainAll, toArray
     @Before
     public void setUp() {
-        tree = new Tree();
-        subTree = tree.addTree(30);
-        assertTrue(tree.add(50));
+        S_tree = new Tree<>();
+        S_subTree = S_tree.addTree(30);
+        assertTrue(S_tree.add(50));
+
+        tree = new Tree<>();
+        subTree1 = tree.addTree(50);
+        subTree2 = tree.addTree(60);
+        subTree1_1 = subTree1.addTree(30);
+        subTree1_1.add(20);
+        subTree2_1 = subTree2.addTree(70);
+        subTree2_2 = subTree2.addTree(80);
     }
 
+    private Tree<Integer> S_tree;
+    private Tree<Integer> S_subTree;
     private Tree<Integer> tree;
-    private Tree<Integer> subTree;
+    private Tree<Integer> subTree1;
+    private Tree<Integer> subTree2;
+    private Tree<Integer> subTree1_1;
+    private Tree<Integer> subTree2_1;
+    private Tree<Integer> subTree2_2;
 
     /**
      * Test the constructor of a Tree.
      *
-     * <li> Implies size == 1 </li>
+     * <li> Implies size == 0 </li>
      * <li> Implies a value of null (due to top node) </li>
      * <li> Implies no children nodes. </li>
      * <li> Implies empty Tree. </li>
@@ -57,40 +71,209 @@ public class TreeTest {
     public void testCheckers() {
 
         //SubTree
-        assertTrue(tree.getSubTree().contains(subTree));
-        assertEquals(tree.getSubTree().size(), tree.childrenSize());
+        assertTrue(S_tree.getSubTree().contains(S_subTree));
+        assertEquals(S_tree.getSubTree().size(), S_tree.childrenSize());
 
         //getValue
-        assertEquals(new Integer(30), subTree.getValue());
-        assertEquals(null, tree.getValue());
+        assertEquals(new Integer(30), S_subTree.getValue());
+        assertEquals(null, S_tree.getValue());
 
         //Contains
-        assertTrue(tree.contains(50));
-        assertTrue(tree.contains(30));
-        assertFalse(subTree.contains(50));
+        assertTrue(S_tree.contains(50));
+        assertTrue(S_tree.contains(30));
+        assertFalse(S_subTree.contains(50));
 
         //Empty
-        assertFalse(tree.isEmpty());
-        assertFalse(subTree.isEmpty());
+        assertFalse(S_tree.isEmpty());
+        assertFalse(S_subTree.isEmpty());
 
         //Size
-        assertEquals(2, tree.size());
-        assertEquals(1, subTree.size());
-        assertEquals(0, subTree.childrenSize());
-        assertEquals(2, tree.childrenSize());
+        assertEquals(2, S_tree.size());
+        assertEquals(1, S_subTree.size());
+        assertEquals(0, S_subTree.childrenSize());
+        assertEquals(2, S_tree.childrenSize());
     }
 
     /**
-     * Test whether clear() on root tree will leave the root tree with no sub nodes.
+     * Test whether clear() on root S_tree will leave the root S_tree with no sub nodes.
      */
     @Test
     public void testClear() {
-        tree.clear();
+        S_tree.clear();
 
-        assertEquals(null, tree.getValue());
-        assertEquals(0, tree.size());
-        assertTrue(tree.contains(null));
-        assertTrue(tree.getSubTree().isEmpty());
+        assertEquals(null, S_tree.getValue());
+        assertEquals(S_tree.size(), 0);
+        assertTrue(S_tree.contains(null));
+        assertTrue(S_tree.getSubTree().isEmpty());
+    }
+
+    /**
+     * Test {@link Tree#remove(java.lang.Object)} and {@link Tree#clearLeftNull()}
+     */
+    @Test
+    public void testRemoveAndClear() {
+        /* Tets remove */
+        //Before
+        assertEquals(tree.size(), 6);
+        assertEquals(tree.sizeObjNb(), 6);
+
+        //remove null
+        assertFalse(tree.remove(null));
+        assertEquals(tree.size(), 6);
+        assertEquals(tree.sizeObjNb(), 6);
+
+        //remove non existing element
+        assertFalse(tree.remove(48));
+        assertEquals(tree.size(), 6);
+        assertEquals(tree.sizeObjNb(), 6);
+
+        //remove S_tree in middle.
+        assertTrue(tree.remove(50));
+        assertFalse(tree.contains(50));
+        assertEquals(tree.size(), 6);
+        assertEquals(tree.sizeObjNb(), 5);
+        assertTrue(tree.contains(30)); //Still contain elements below
+        assertTrue(tree.contains(20)); //Still contain elements below
+
+        //Try removing the new null element (no changes expected)
+        assertFalse(tree.remove(null));
+        assertEquals(6, tree.size());
+        assertEquals(5, tree.sizeObjNb());
+
+        /* Test clearLeftNull */
+        //Clear dangling nulls (no changes expected)
+        tree.clearLeftNull();
+        assertEquals(6, tree.size());
+        assertEquals(5, tree.sizeObjNb());
+
+        //Remove 2 subTrees and clear dangling nulls (size decrease)
+        assertTrue(tree.remove(30));
+        assertTrue(tree.remove(20));
+        tree.clearLeftNull();
+        assertFalse(tree.contains(30));
+        assertFalse(tree.contains(20));
+        assertEquals(3, tree.size());
+        assertEquals(3, tree.sizeObjNb());
+    }
+
+    @Test
+    public void testRemoveTree() {
+        //Before
+        assertEquals(6, tree.size());
+        assertEquals(6, tree.sizeObjNb());
+
+        assertFalse(tree.removeTree(null)); //remove null
+        assertEquals(6, tree.size());
+        assertEquals(6, tree.sizeObjNb());
+
+        assertFalse(tree.removeTree(48)); //remove non existing element
+        assertEquals(6, tree.size());
+        assertEquals(6, tree.sizeObjNb());
+
+        assertTrue(tree.removeTree(50)); //remove S_tree in middle.
+        assertFalse(tree.contains(50));
+        assertEquals(3, tree.size());
+        assertEquals(3, tree.sizeObjNb());
+        assertFalse(tree.contains(30)); //no elements below
+        assertFalse(tree.contains(20)); //no elements below
+
+        assertFalse(tree.removeTree(null));
+        assertEquals(3, tree.size());
+        assertEquals(3, tree.sizeObjNb());
+    }
+
+    /**
+     * Test {@link Tree#iterator()} using {@link Iterator#next()} and {@link Iterator#hasNext()} to see if every element
+     * returned is in the Tree and if the amount of elements is the same with the amount in the tree.
+     */
+    @Test
+    public void testIterator() {
+        Iterator<Integer> iterator = tree.iterator();
+        int counter = 0;
+
+        while (iterator.hasNext()) {
+            int ele = iterator.next();
+            counter++;
+            assertTrue(tree.contains(ele));
+        }
+
+        assertEquals(tree.size(), counter);
+    }
+
+    /**
+     * Test {@link Tree#iterator()} using {@link Iterator#next()} to have the depth-first pattern.
+     */
+    @Test
+    public void testIteratorNext() {
+        Iterator<Integer> iterator = tree.iterator();
+
+        //50, 30, 20, 60, 70, 80
+        assertEquals((int) 50, (int) iterator.next());
+        assertEquals((int) 30, (int) iterator.next());
+        assertEquals((int) 20, (int) iterator.next());
+        assertEquals((int) 60, (int) iterator.next());
+        assertEquals((int) 70, (int) iterator.next());
+        assertEquals((int) 80, (int) iterator.next());
+    }
+
+    /**
+     * Test {@link Tree#iterator()} using {@link Iterator#next()} causing a NoSuchElementException
+     */
+    @Test(expected = NoSuchElementException.class)
+    public void testIteratorNextNoSuchElement() {
+        Tree tree = new Tree();
+        tree.add(50);
+        Iterator<Integer> iterator = tree.iterator();
+        assertEquals((int) 50, (int) iterator.next());
+        iterator.next();
+    }
+    
+    
+    /**
+     * Test {@link Tree#iterator()} using {@link Iterator#next()} and {@link Iterator#hasNext()} to see if every element
+     * returned is in the Tree and if the amount of elements is the same with the amount in the tree.
+     */
+    @Test
+    public void testNodeIterator() {
+        Iterator<Tree<Integer>> iterator = tree.nodeIterator();
+        int counter = 0;
+
+        while (iterator.hasNext()) {
+            Tree<Integer> node = iterator.next();
+            counter++;
+            assertTrue(tree.contains(node));
+            assertTrue(tree.contains(node.getValue()));
+        }
+
+        assertEquals(tree.size(), counter);
+    }
+
+    /**
+     * Test {@link Tree#nodeIterator()} using {@link Iterator#next()} to have the depth-first pattern.
+     */
+    @Test
+    public void testNodeIteratorNext() {
+        Iterator<Tree<Integer>> iterator = tree.nodeIterator();
+
+        //50, 30, 20, 60, 70, 80
+        assertEquals((int) 50, (int) iterator.next().getValue());
+        assertEquals((int) 30, (int) iterator.next().getValue());
+        assertEquals((int) 20, (int) iterator.next().getValue());
+        assertEquals((int) 60, (int) iterator.next().getValue());
+        assertEquals((int) 70, (int) iterator.next().getValue());
+        assertEquals((int) 80, (int) iterator.next().getValue());
+    }
+
+    /**
+     * Test {@link Tree#nodeIterator()} using {@link Iterator#next()} causing a NoSuchElementException
+     */
+    @Test(expected = NoSuchElementException.class)
+    public void testNodeIteratorNextNoSuchElement() {
+        Tree<Integer> tree = new Tree<>();
+        tree.add(50);
+        Iterator<Tree<Integer>> iterator = tree.nodeIterator();
+        assertEquals((int) 50, (int) iterator.next().getValue());
+        iterator.next();
     }
 
 }
