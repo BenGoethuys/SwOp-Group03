8b69a8cd55d1c80e573966f9786f0afdcad198a2
VincentDerk <vincent.derkinderen@student.kuleuven.be>
2016-04-03 20:32:09 +0200

    Created the start of 2 new use cases.

---
+++ BugTrap/BugTrap/src/bugtrap03/gui/cmd/ProposePatchCmd.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bugtrap03.gui.cmd;
+
+import bugtrap03.bugdomain.permission.PermissionException;
+import bugtrap03.bugdomain.usersystem.User;
+import bugtrap03.gui.cmd.general.CancelException;
+import bugtrap03.gui.terminal.TerminalScanner;
+import bugtrap03.model.DataModel;
+
+/**
+ *
+ * @author Admin
+ */
+public class ProposePatchCmd implements Cmd {
+
+    //TODO: Implement
+    
+    @Override
+    public Object exec(TerminalScanner scan, DataModel model, User user) throws PermissionException, CancelException, IllegalArgumentException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
