0431bb8f46fdd0c15c753bdab78bdb97522b6016
KwintenBuytaert <kwinten.buytaert@hotmail.com>
2016-04-07 20:39:38 +0200

    added TODO/FIX ME used equalsIgnoreCase

--- BugTrap/BugTrap/src/bugtrap03/gui/cmd/RegisterForNotificationsCmd.java
+++ BugTrap/BugTrap/src/bugtrap03/gui/cmd/RegisterForNotificationsCmd.java
@@ -1,10 +1,14 @@
 package bugtrap03.gui.cmd;
 
+import bugtrap03.bugdomain.Project;
+import bugtrap03.bugdomain.Subsystem;
+import bugtrap03.bugdomain.bugreport.BugReport;
 import bugtrap03.bugdomain.permission.PermissionException;
 import bugtrap03.bugdomain.usersystem.Issuer;
 import bugtrap03.bugdomain.usersystem.User;
 import bugtrap03.gui.cmd.general.CancelException;
 import bugtrap03.gui.cmd.general.GetObjectOfListCmd;
+import bugtrap03.gui.cmd.general.GetProjectCmd;
 import bugtrap03.gui.terminal.TerminalScanner;
 import bugtrap03.model.DataModel;
 import purecollections.PList;
@@ -34,6 +38,8 @@ public class RegisterForNotificationsCmd implements Cmd {
         if (scan == null || model == null) {
             throw new IllegalArgumentException("scan, model and user musn't be null.");
         }
+        //TODO check for issuer?
+        //TODO if so check using userPermission
         if (! (user instanceof Issuer)){
             throw new IllegalArgumentException("Precondition not met: logged in user must be an issuer");
         }
@@ -43,16 +49,25 @@ public class RegisterForNotificationsCmd implements Cmd {
         subjectypes.add("Bugreport");
         scan.println("Select subject type.");
         String subjectype = new GetObjectOfListCmd<>(PList.<String>empty().plusAll(subjectypes),
-                u -> (u.toString()), ((u, input) -> ((u.toLowerCase().equals(input.toLowerCase()))))).exec(scan, model, null);
+                u -> (u.toString()), ((u, input) -> ((u.equalsIgnoreCase(input))))).exec(scan, model, null);
         subjectype = subjectype.toLowerCase();
+        scan.println("Select project.");
+        Project selectedProj = new GetProjectCmd().exec(scan, model, user);
         if (subjectype.equals("project")){
 
         } else if (subjectype.equals("subsystem")){
+            scan.println("Select subsystem.");
+            PList<Subsystem> allSubsystems = model.getAllSubsystems(selectedProj);
+            Subsystem selectedSub = new GetObjectOfListCmd<>(allSubsystems, (u -> u.getName()),
+                    ((u, input) -> u.getName().equalsIgnoreCase(input)))
+                    .exec(scan, model, user);
 
         } else if (subjectype.equals("bugreport")){
+            scan.println("Select bug report.");
+            BugReport selectedBug = new SelectBugReportCmd().exec(scan, model, user);
 
         } else {
-
+            throw new IllegalArgumentException("Something went wrong while selection subjecttype");
         }
         return null;
     }
