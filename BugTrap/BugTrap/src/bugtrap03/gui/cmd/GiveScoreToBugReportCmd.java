05fbcef5993c2b61769a922b67998284659e7345
BenGoethuys <ben.goethuys@gmail.com>
2016-04-11 15:52:28 +0200

    Changed comment + select bug report when none was given

--- BugTrap/BugTrap/src/bugtrap03/gui/cmd/GiveScoreToBugReportCmd.java
+++ BugTrap/BugTrap/src/bugtrap03/gui/cmd/GiveScoreToBugReportCmd.java
@@ -18,14 +18,16 @@ public class GiveScoreToBugReportCmd implements Cmd {
      * This is the constructor for this cmd
      *
      * @param bugReport The bug report that will have a score change
-     *
-     * @throws IllegalArgumentException If the given bug report was invalid, thus null.
      */
     public GiveScoreToBugReportCmd(BugReport bugReport) throws IllegalArgumentException {
-        if (bugReport == null){
-            throw new IllegalArgumentException("The given bug report to give a score, cannot be null");
-        }
         this.bugReport = bugReport;
+    }
+
+    /**
+     * This is the basic constructor
+     */
+    public GiveScoreToBugReportCmd() {
+        this.bugReport = null;
     }
 
     private BugReport bugReport;
@@ -36,7 +38,9 @@ public class GiveScoreToBugReportCmd implements Cmd {
      * @param scan  The scanner used to interact with the person.
      * @param model The model used for model access.
      * @param user  The {@link User} who wants to executes this command.
-     * @return null if there is no result specified.
+     *
+     * @return the selected int;
+     *
      * @throws PermissionException      When the user does not have sufficient
      *                                  permissions.
      * @throws CancelException          When the users wants to abort the current cmd
@@ -45,6 +49,12 @@ public class GiveScoreToBugReportCmd implements Cmd {
      */
     @Override
     public Integer exec(TerminalScanner scan, DataModel model, User user) throws PermissionException, CancelException, IllegalArgumentException {
+        // if bug report was not defined:
+        if (this.bugReport == null){
+            this.bugReport = new SelectBugReportCmd().exec(scan, model, user);
+        }
+
+        // ask user for score
         scan.println("Give a score between 1 and 5: ");
         int score = new GetIntCmd().exec(scan, model, user);
         this.bugReport.giveScore(user, score);
