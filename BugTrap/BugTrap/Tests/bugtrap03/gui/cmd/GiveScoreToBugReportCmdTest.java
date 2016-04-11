b76007690d57533c0dfbcbc53b66bd3be4c5a0ab
Mathias Dekempeneer <mathiasdekempeneer@gmail.com>
2016-04-11 16:38:09 +0200

    Start of test give score

---
+++ BugTrap/BugTrap/Tests/bugtrap03/gui/cmd/GiveScoreToBugReportCmdTest.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package bugtrap03.gui.cmd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Mathias
+ *
+ */
+public class GiveScoreToBugReportCmdTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * Test method for {@link bugtrap03.gui.cmd.GiveScoreToBugReportCmd#GiveScoreToBugReportCmd(bugtrap03.bugdomain.bugreport.BugReport)}.
+     */
+    @Test
+    public void testGiveScoreToBugReportCmdBugReport() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link bugtrap03.gui.cmd.GiveScoreToBugReportCmd#GiveScoreToBugReportCmd()}.
+     */
+    @Test
+    public void testGiveScoreToBugReportCmd() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link bugtrap03.gui.cmd.GiveScoreToBugReportCmd#exec(bugtrap03.gui.terminal.TerminalScanner, bugtrap03.model.DataModel, bugtrap03.bugdomain.usersystem.User)}.
+     */
+    @Test
+    public void testExec() {
+        fail("Not yet implemented");
+    }
+
+}
