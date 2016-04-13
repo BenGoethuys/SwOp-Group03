0018c9ce22c36fca81e50edba74535db43c2be73
Mathias Dekempeneer <mathiasdekempeneer@gmail.com>
2016-04-13 14:02:44 +0200

    Start of test SetDuplicateBugReportCmd

---
+++ BugTrap/BugTrap/Tests/bugtrap03/gui/cmd/SetDuplicateBugReportCmdTest.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package bugtrap03.gui.cmd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Mathias
+ *
+ */
+public class SetDuplicateBugReportCmdTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * Test method for {@link bugtrap03.gui.cmd.SetDuplicateBugReportCmd#SetDuplicateBugReportCmd(bugtrap03.bugdomain.bugreport.BugReport)}.
+     */
+    @Test
+    public void testSetDuplicateBugReportCmdBugReport() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link bugtrap03.gui.cmd.SetDuplicateBugReportCmd#SetDuplicateBugReportCmd()}.
+     */
+    @Test
+    public void testSetDuplicateBugReportCmd() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link bugtrap03.gui.cmd.SetDuplicateBugReportCmd#exec(bugtrap03.gui.terminal.TerminalScanner, bugtrap03.model.DataModel, bugtrap03.bugdomain.usersystem.User)}.
+     */
+    @Test
+    public void testExec() {
+        fail("Not yet implemented");
+    }
+
+}
