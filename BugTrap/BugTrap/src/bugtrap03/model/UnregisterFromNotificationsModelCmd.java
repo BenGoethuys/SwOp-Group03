e436936f773bc123d51d6f114302047858529149
KwintenB <kwinten.buytaert@hotmail.com>
2016-04-13 12:17:25 +0200

    unsubscribe edited to work down multiple layers of mailboxes

--- BugTrap/BugTrap/src/bugtrap03/model/UnregisterFromNotificationsModelCmd.java
+++ BugTrap/BugTrap/src/bugtrap03/model/UnregisterFromNotificationsModelCmd.java
@@ -3,6 +3,7 @@ package bugtrap03.model;
 import bugtrap03.bugdomain.notification.Mailbox;
 import bugtrap03.bugdomain.permission.PermissionException;
 import bugtrap03.bugdomain.usersystem.User;
+import purecollections.PList;
 
 /**
  * @author Group 03
@@ -13,14 +14,16 @@ public class UnregisterFromNotificationsModelCmd extends ModelCmd{
             throw new IllegalArgumentException("Invalid User for unregistration from notifications.");
         }
         this.unsubscriber = user;
-        if (! isValidMailbox(mailbox)){
+        if (! isValidMailbox(mailbox, user)){
             throw new IllegalArgumentException("Invalid Mailbox for unregistration from notifiactions");
         }
         this.mailbox = mailbox;
+        this.isExecuted = false;
     }
 
     private User unsubscriber;
     private Mailbox mailbox;
+    private boolean isExecuted;
 
     /**
      * This method checks the validity of a given user.
@@ -37,10 +40,14 @@ public class UnregisterFromNotificationsModelCmd extends ModelCmd{
     /**
      * This method checks the validity of a given user.
      * @param mailbox The user to check.
-     * @return True if the user is not null.
+     * @return True if the user is not null or the given mailox cannot be found in the all the mailboxes from the user.
      */
-    public boolean isValidMailbox(Mailbox mailbox){
+    public boolean isValidMailbox(Mailbox mailbox, User user){
         if (mailbox == null){
+            return false;
+        }
+        PList<Mailbox> allBoxes = user.getMailbox().getAllBoxes();
+        if (! allBoxes.contains(mailbox)){
             return false;
         }
         return true;
@@ -48,17 +55,23 @@ public class UnregisterFromNotificationsModelCmd extends ModelCmd{
 
     @Override
     Object exec() throws IllegalArgumentException, NullPointerException, PermissionException, IllegalStateException {
+
+        this.isExecuted = true;
         return null;
     }
 
     @Override
     boolean undo() {
-        return false;
+        if (! isExecuted()){
+            return false;
+        }
+        this.unsubscriber.getMailbox().addBox(this.mailbox);
+        return true;
     }
 
     @Override
     boolean isExecuted() {
-        return false;
+        return this.isExecuted;
     }
 
     @Override
