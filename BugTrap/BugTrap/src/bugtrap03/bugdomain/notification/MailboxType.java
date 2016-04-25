13c5ece2c13f1812b717113d15a7ad7da457074c
KwintenB <kwinten.buytaert@hotmail.com>
2016-04-25 16:12:56 +0200

    `getMBTypeInfo`

--- BugTrap/BugTrap/src/bugtrap03/bugdomain/notification/MailboxType.java
+++ BugTrap/BugTrap/src/bugtrap03/bugdomain/notification/MailboxType.java
@@ -3,5 +3,18 @@ package bugtrap03.bugdomain.notification;
 /**
  * Created by Kwinten on 25/04/2016.
  */
-public class MailboxType {
+public enum MailboxType {
+    CREATION_BUGREP{
+        @Override
+        public String getMBTypeInfo(){
+            return "the creation of bugreports";
+        }
+    },
+    VERSIONID_UPDATE{
+        @Override
+        public String getMBTypeInfo(){
+            return "the update of the VersionID";
+        }
+    };
+    public abstract String getMBTypeInfo();
 }
