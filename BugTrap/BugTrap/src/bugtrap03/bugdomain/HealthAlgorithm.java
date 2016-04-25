6b02e422e3f0e12db58d5a15c0d15a782db26482
Mathias Dekempeneer <mathiasdekempeneer@gmail.com>
2016-04-25 17:00:54 +0200

    Added abstract methods to healthAlgorithm

--- BugTrap/BugTrap/src/bugtrap03/bugdomain/HealthAlgorithm.java
+++ BugTrap/BugTrap/src/bugtrap03/bugdomain/HealthAlgorithm.java
@@ -5,18 +5,28 @@ package bugtrap03.bugdomain;
  *
  */
 public abstract class HealthAlgorithm {
-	
-	public HealthIndicator getIndicator() {
-		
-		return null;
+
+	public HealthIndicator getIndicator(Subsystem subsystem) {
+		if (isHealthy(subsystem)) {
+			return HealthIndicator.HEALTY;
+		}
+		if (isSatisfactory(subsystem)) {
+			return HealthIndicator.SATISFACTORY;
+		}
+		if (isStable(subsystem)) {
+			return HealthIndicator.STABLE;
+		}
+		if (isSerious(subsystem)) {
+			return HealthIndicator.SERIOUS;
+		}
+		return HealthIndicator.CRITICAL;
 	}
-	
-	
-	public abstract boolean isHealthy();
-	public abstract boolean isSatisfactory();
-	public abstract boolean isStable();
-	public abstract boolean isSerious();
-	public abstract boolean isCritical();
-	
-	
+
+	public abstract boolean isHealthy(Subsystem subsystem);
+
+	public abstract boolean isSatisfactory(Subsystem subsystem);
+
+	public abstract boolean isStable(Subsystem subsystem);
+
+	public abstract boolean isSerious(Subsystem subsystem);
 }
