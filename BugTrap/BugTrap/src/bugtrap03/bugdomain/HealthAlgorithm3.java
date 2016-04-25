71f812d5f1c7491cfc6b2e0aa6807d5f18e5e69f
Mathias Dekempeneer <mathiasdekempeneer@gmail.com>
2016-04-25 17:05:21 +0200

    Algorithm 3

--- BugTrap/BugTrap/src/bugtrap03/bugdomain/HealthAlgorithm3.java
+++ BugTrap/BugTrap/src/bugtrap03/bugdomain/HealthAlgorithm3.java
@@ -9,35 +9,70 @@ package bugtrap03.bugdomain;
  */
 public class HealthAlgorithm3 extends HealthAlgorithm {
 
+	private final int HEALTH = 10;
+	private final int SATISFACTORY = 100;
+	private final int STABLE = 250;
+	private final int SERIOUS = 500;
+
 	@Override
-	public boolean isHealthy() {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean isHealthy(Subsystem subsystem) {
+
+		for (Subsystem subs : subsystem.getAllSubsystems()) {
+			if (subs.getIndicator().getOrder() < HealthIndicator.HEALTY.getOrder()) {
+				return false;
+			}
+		}
+
+		if (subsystem.getBugImpact() >= HEALTH) {
+			return false;
+		}
+
+		return true;
 	}
 
 	@Override
-	public boolean isSatisfactory() {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean isSatisfactory(Subsystem subsystem) {
+		for (Subsystem subs : subsystem.getAllSubsystems()) {
+			if (subs.getIndicator().getOrder() < HealthIndicator.STABLE.getOrder()) {
+				return false;
+			}
+		}
+
+		if (subsystem.getBugImpact() >= SATISFACTORY) {
+			return false;
+		}
+
+		return true;
 	}
 
 	@Override
-	public boolean isStable() {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean isStable(Subsystem subsystem) {
+		for (Subsystem subs : subsystem.getAllSubsystems()) {
+			if (subs.getIndicator().getOrder() < HealthIndicator.STABLE.getOrder()) {
+				return false;
+			}
+		}
+
+		if (subsystem.getBugImpact() >= STABLE) {
+			return false;
+		}
+
+		return true;
 	}
 
 	@Override
-	public boolean isSerious() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	public boolean isSerious(Subsystem subsystem) {
+		for (Subsystem subs : subsystem.getAllSubsystems()) {
+			if (subs.getIndicator().getOrder() < HealthIndicator.SERIOUS.getOrder()) {
+				return false;
+			}
+		}
 
-	@Override
-	public boolean isCritical() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+		if (subsystem.getBugImpact() >= SERIOUS) {
+			return false;
+		}
 
+		return true;
+	}
 
 }
