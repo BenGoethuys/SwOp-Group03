275690932588cbd54983055e6c9375d1e7845544
VincentDerk <vincent.derkinderen@student.kuleuven.be>
2016-04-27 11:44:20 +0200

    Added Mementos to AbstractSystem, Project and Subsystem.

---
+++ BugTrap/BugTrap/src/bugtrap03/bugdomain/ProjectMemento.java
@@ -0,0 +1,59 @@
+package bugtrap03.bugdomain;
+
+import bugtrap03.bugdomain.usersystem.Developer;
+import bugtrap03.bugdomain.usersystem.Role;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import purecollections.PList;
+
+/**
+ * A (partial) memento of the project.
+ * <br> This stores everything an {@link AbstractSystemMemento} does as well as
+ * creationDate (clone), startDate (clone), budgetEsimate, isTerminated and the projectParticipants (clone).
+ *
+ * @author Group 03
+ */
+public class ProjectMemento extends AbstractSystemMemento {
+
+    ProjectMemento(VersionID versionID, String name, String description, PList<Subsystem> children, 
+            AbstractSystem parent, Milestone milestone, GregorianCalendar creationDate, GregorianCalendar startDate,
+            HashMap<Developer, PList<Role>> projectParticipants, long budgetEstimate, boolean isTerminated) {
+        super(versionID, name, description, children, parent, milestone);
+        
+        this.creationDate = (GregorianCalendar) creationDate.clone();
+        this.startDate = (GregorianCalendar) startDate.clone();
+        
+        this.projectParticipants = (HashMap<Developer, PList<Role>>) projectParticipants.clone();
+        
+        this.budgetEstimate = budgetEstimate;
+        this.isTerminated = isTerminated;
+    }
+
+    private final GregorianCalendar creationDate;
+    private final GregorianCalendar startDate;
+    private final HashMap<Developer, PList<Role>> projectParticipants;
+    private long budgetEstimate;
+
+    private boolean isTerminated;
+    
+    GregorianCalendar getCreationDate() {
+        return this.creationDate;
+    }
+    
+    GregorianCalendar getStartDate() {
+        return this.startDate;
+    }
+    
+    HashMap<Developer, PList<Role>> getProjectParticipants() {
+        return this.projectParticipants;
+    }
+    
+    long getBudgetEstimate() {
+        return this.budgetEstimate;
+    }
+    
+    boolean getIsTerminated() {
+        return this.isTerminated;
+    }
+
+}
---
+++ BugTrap/BugTrap/src/bugtrap03/bugdomain/SubsystemMemento.java
@@ -0,0 +1,49 @@
+package bugtrap03.bugdomain;
+
+import bugtrap03.bugdomain.bugreport.BugReport;
+import bugtrap03.bugdomain.bugreport.BugReportMemento;
+import java.util.HashMap;
+import purecollections.PList;
+
+/**
+ * A (partial) memento of the subsystem.
+ * <p>
+ * <br> This stores the versionID, name, description, children (and their state), parent (excl state) and milestone,
+ * direct bugReports (and their state).
+ *
+ * @author Group 03
+ */
+public class SubsystemMemento extends AbstractSystemMemento {
+
+    SubsystemMemento(VersionID versionID, String name, String description, PList<Subsystem> children, AbstractSystem parent, Milestone milestone, PList<BugReport> bugReports) {
+        super(versionID, name, description, children, parent, milestone);
+
+        this.bugReportList = bugReports;
+
+        bugReportMementos = new HashMap<>();
+        for (BugReport bugReport : bugReports) {
+            bugReportMementos.put(bugReport, bugReport.getMemento());
+        }
+    }
+
+    private final PList<BugReport> bugReportList;
+    private final HashMap<BugReport, BugReportMemento> bugReportMementos;
+
+    PList<BugReport> getBugReportList() {
+        return this.bugReportList;
+    }
+
+    /**
+     * Restore the state of the direct bug Reports by restoring their memento.
+     */
+    void restoreBugReports() {
+        for (BugReport bugRep : bugReportList) {
+            BugReportMemento mem = bugReportMementos.get(bugRep);
+
+            if (mem != null) {
+                bugRep.setMemento(mem);
+            }
+        }
+    }
+
+}
