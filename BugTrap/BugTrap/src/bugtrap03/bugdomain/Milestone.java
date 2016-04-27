b4b11184afe2745387e79d16f0d06482e1bea233
Mathias Dekempeneer <mathiasdekempeneer@gmail.com>
2016-04-27 17:26:51 +0200

    Changed the tests to the new Milestone

--- BugTrap/BugTrap/src/bugtrap03/bugdomain/Milestone.java
+++ BugTrap/BugTrap/src/bugtrap03/bugdomain/Milestone.java
@@ -20,33 +20,12 @@ public class Milestone extends VersionID {
      * @see VersionID#VersionID(int, int, int)
      */
     @DomainAPI
-    public Milestone(int first, int second, int third) {
-        super(first, second, third);
+    public Milestone(int... nb) {
+        super(nb);
     }
 
-    /**
-     * Creates a Milestone
-     * 
-     * @param first The first digit of the Milestone.
-     * @param second The second digit of the Milestone.
-     * 
-     * @see VersionID#VersionID(int, int)
-     */
-    @DomainAPI
-    public Milestone(int first, int second) {
-        super(first, second);
-    }
-
-    /**
-     * Creates a Milestone
-     * 
-     * @param first The first digit of the Milestone.
-     * 
-     * @see VersionID#VersionID(int)
-     */
-    @DomainAPI
-    public Milestone(int first) {
-        super(first);
+    public Milestone() {
+	super();
     }
 
     /**
@@ -55,62 +34,15 @@ public class Milestone extends VersionID {
     @Override
     @DomainAPI
     public String toString() {
-        if (this.getThirdNb() != 0) {
-            return "M" + this.getFirstNb() + "." + this.getSecondNb() + "." + this.getThirdNb();
-        } else if (this.getSecondNb() != 0) {
-            return "M" + this.getFirstNb() + "." + this.getSecondNb();
-        } else {
-            return "M" + this.getFirstNb();
-        }
-    }
+	String milestone = "M";
+	int[] numbers = this.getNumbers();
+	int max = numbers.length - 1;
+	for (int i = 0; i < max; i++) {
+	    milestone += Integer.toString(numbers[i]);
+	    milestone += ".";
+	}
+	milestone += Integer.toString(numbers[max]);
 
-    /**
-     * Compares two Milestones.
-     *
-     * @return 0 if and only if two Milestones are equal, 1 if and only if the
-     *         second Milestone is greater than the first Milestone, -1
-     *         otherwise.
-     */
-    @DomainAPI
-    public int compareTo(Milestone other) {
-        if (this.getFirstNb() < other.getFirstNb()) {
-            return -1;
-        }
-        if (this.getFirstNb() > other.getFirstNb()) {
-            return 1;
-        }
-        if (this.getSecondNb() < other.getSecondNb()) {
-            return -1;
-        }
-        if (this.getSecondNb() > other.getSecondNb()) {
-            return 1;
-        }
-        if (this.getThirdNb() < other.getThirdNb()) {
-            return -1;
-        }
-        if (this.getThirdNb() > other.getThirdNb()) {
-            return 1;
-        }
-        return 0;
+	return milestone;
     }
-
-    /**
-     * Checks if two Milestones are equal.
-     *
-     * @param other The Milestone to compare.
-     * @return True if the Milestones are equal.
-     */
-    @Override
-    @DomainAPI
-    public boolean equals(Object other) {
-        if (other == null) {
-            return false;
-        }
-        if (other instanceof Milestone) {
-            Milestone otherID = (Milestone) other;
-            return this.compareTo(otherID) == 0;
-        }
-        return false;
-    }
-
 }
