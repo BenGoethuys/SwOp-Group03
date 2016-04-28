e197671dcd48e4f430967c7bd68060798eb953e5
KwintenB <kwinten.buytaert@hotmail.com>
2016-04-28 22:58:15 +0200

    added List for milestonsubs and versionid subs, -> constructor and getters

--- BugTrap/BugTrap/src/bugtrap03/bugdomain/notificationdomain/AbstractSystemSubjectMemento.java
+++ BugTrap/BugTrap/src/bugtrap03/bugdomain/notificationdomain/AbstractSystemSubjectMemento.java
@@ -1,9 +1,7 @@
 package bugtrap03.bugdomain.notificationdomain;
 
 import bugtrap03.bugdomain.notificationdomain.SubjectMemento;
-import bugtrap03.bugdomain.notificationdomain.mailboxes.CommentMailBox;
-import bugtrap03.bugdomain.notificationdomain.mailboxes.CreationMailBox;
-import bugtrap03.bugdomain.notificationdomain.mailboxes.TagMailBox;
+import bugtrap03.bugdomain.notificationdomain.mailboxes.*;
 import purecollections.PList;
 
 /**
@@ -12,16 +10,37 @@ import purecollections.PList;
  */
 public class AbstractSystemSubjectMemento extends SubjectMemento {
     
-    protected AbstractSystemSubjectMemento(PList<TagMailBox> tagSubs, PList<CommentMailBox> commentSubs, PList<CreationMailBox> creationSubs) {
+    protected AbstractSystemSubjectMemento(PList<TagMailBox> tagSubs, PList<CommentMailBox> commentSubs, PList<CreationMailBox> creationSubs,
+                                           PList<MilestoneMailbox> milestoneSubs, PList<VersionIDMailbox> versionIDSubs) {
         super(tagSubs, commentSubs);
         
         this.creationSubs = creationSubs;
+        this.milestoneSubs = milestoneSubs;
+        this.versionIDSubs = versionIDSubs;
     }
     
     private final PList<CreationMailBox> creationSubs;
-    
+    private final PList<MilestoneMailbox> milestoneSubs;
+    private final PList<VersionIDMailbox> versionIDSubs;
+
+    /**
+     * This method returns the static list of creationsubs.
+     * @return A Plist of creation mailboxes.
+     */
     PList<CreationMailBox> getCreationSubs() {
         return this.creationSubs;
     }
+
+    /**
+     * This method returns the static list of milestonesubs.
+     * @return A Plist of mileston mailboxes.
+     */
+    PList<MilestoneMailbox> getMilestoneSubs() { return this.milestoneSubs;}
+
+    /**
+     * This method returns the static list of versionIDsubs.
+     * @return A Plist of versionID mailboxes.
+     */
+    PList<VersionIDMailbox> getVersionIDSubs(){ return this.versionIDSubs;}
     
 }
