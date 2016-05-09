f3b661ccf795a54cad05fb062f41c9f2cd68e709
KwintenB <kwinten.buytaert@hotmail.com>
2016-05-09 17:36:29 +0200

    implemented methods

---
+++ BugTrap/BugTrap/src/bugtrap03/model/RegisterForMilestoneNotificationsModelCmd.java
@@ -0,0 +1,64 @@
+package bugtrap03.model;
+
+import bugtrap03.bugdomain.AbstractSystem;
+import bugtrap03.bugdomain.Milestone;
+import bugtrap03.bugdomain.notificationdomain.AbstractSystemSubject;
+import bugtrap03.bugdomain.notificationdomain.mailboxes.MilestoneMailbox;
+import bugtrap03.bugdomain.permission.PermissionException;
+import bugtrap03.bugdomain.usersystem.User;
+import bugtrap03.gui.cmd.RegisterForNotificationsCmd;
+
+/**
+ * Created by Kwinten on 09/05/2016.
+ */
+public class RegisterForMilestoneNotificationsModelCmd extends RegisterForNotificationsModelCmd {
+
+    /**
+     * Create a {@link ModelCmd} that subscribes to the given subject
+     * for the change of milestone on abstract systems
+     * @param user The user that wishes to subscribe
+     * @param subject The subject on which the user wishes to subscribe
+     * @param milestone The milestone on for which the user wishes to subscribe
+     *
+     * @throws IllegalArgumentException When the given subject is null
+     * @throws IllegalArgumentException When the given subject is Terminated
+     */
+    RegisterForMilestoneNotificationsModelCmd(User user, AbstractSystemSubject subject, Milestone milestone) throws IllegalArgumentException {
+        super(user);
+        if (subject == null) {
+            throw new IllegalArgumentException("The given subject is null");
+        }
+        if (subject.isTerminated()){
+            throw new IllegalArgumentException("The given subject is terminated");
+        }
+        this.subject = subject;
+        this.milestone = milestone;
+    }
+
+    private AbstractSystemSubject subject;
+    private Milestone milestone;
+
+    /**
+     * This method executes this model command.
+     * @return The created TagMailBox representing the subscription that contains the notifications
+     * @throws IllegalArgumentException If subject is terminated
+     * @throws IllegalStateException if the state of this command is invalid
+     * @see bugtrap03.bugdomain.notificationdomain.mailboxes.Mailbox#milestoneSubscribe(AbstractSystemSubject)
+     * @see #setExecuted()
+     */
+    @Override
+    MilestoneMailbox exec() throws IllegalArgumentException, IllegalStateException {
+        if (subject.isTerminated()) {
+            throw new IllegalArgumentException("The given subject is terminated.");
+        }
+        this.setExecuted();
+        MilestoneMailbox mmb;
+        if (this.milestone == null){
+            mmb = this.getMailbox().milestoneSubscribe(this.subject);
+        } else {
+            mmb = this.getMailbox().milestoneSubscribe(this.subject, this.milestone);
+        }
+        this.setNewMailbox(mmb);
+        return mmb;
+    }
+}
