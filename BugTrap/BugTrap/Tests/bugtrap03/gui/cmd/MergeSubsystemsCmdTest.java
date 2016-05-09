25b36fcb2058c3b5c34da3b35965b13912c56b22
Mathias Dekempeneer <mathiasdekempeneer@gmail.com>
2016-05-09 15:14:12 +0200

    Start of tests Merge and Split subsystem

---
+++ BugTrap/BugTrap/Tests/bugtrap03/gui/cmd/MergeSubsystemsCmdTest.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package bugtrap03.gui.cmd;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Mathias
+ *
+ */
+public class MergeSubsystemsCmdTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    /**
+     * Test method for {@link bugtrap03.gui.cmd.MergeSubsystemsCmd#exec(bugtrap03.gui.terminal.TerminalScanner, bugtrap03.model.DataModel, bugtrap03.bugdomain.usersystem.User)}.
+     */
+    @Test
+    public void testExec() {
+	fail("Not yet implemented");
+    }
+
+}
