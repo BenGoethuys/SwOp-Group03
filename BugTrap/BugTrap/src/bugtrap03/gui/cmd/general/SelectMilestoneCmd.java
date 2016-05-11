ba4618776cd53b158b230e8222dd0427a444065c
KwintenB <kwinten.buytaert@hotmail.com>
2016-05-11 14:13:21 +0200

    added general cmd for selecting a milestone

---
+++ BugTrap/BugTrap/src/bugtrap03/gui/cmd/general/SelectMilestoneCmd.java
@@ -0,0 +1,35 @@
+package bugtrap03.gui.cmd.general;
+
+import bugtrap03.bugdomain.Milestone;
+import bugtrap03.bugdomain.permission.PermissionException;
+import bugtrap03.bugdomain.usersystem.User;
+import bugtrap03.gui.cmd.Cmd;
+import bugtrap03.gui.terminal.TerminalScanner;
+import bugtrap03.model.DataModel;
+
+/**
+ * @author Group 03
+ */
+public class SelectMilestoneCmd implements Cmd<Milestone> {
+    @Override
+    public Milestone exec(TerminalScanner scan, DataModel modelDummy, User userDummy) throws CancelException {
+        Milestone bugReportMilestone = null;
+        scan.print("Enter a new milestone: (format a.b.c) ");
+        do {
+            String input = scan.nextLine();
+            String[] milestoneStr = input.split("\\.");
+
+            int nb1, nb2, nb3;
+            try {
+                nb1 = Integer.parseInt(milestoneStr[0]);
+                nb2 = Integer.parseInt(milestoneStr[1]);
+                nb3 = Integer.parseInt(milestoneStr[2]);
+
+                bugReportMilestone = new Milestone(nb1, nb2, nb3);
+            } catch (IndexOutOfBoundsException | NumberFormatException ex) {
+                scan.println("Invalid input. Please try again using format: a.b.c");
+            }
+        } while (bugReportMilestone == null);
+        return bugReportMilestone;
+    }
+}
