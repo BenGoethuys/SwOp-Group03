19bf876b8f858cfd79c02c0e8a3ef95ad6e78184
Mathias Dekempeneer <mathiasdekempeneer@gmail.com>
2016-05-11 15:53:12 +0200

    Changed DeclareAchievedMilestone with multiple digits

--- BugTrap/BugTrap/src/bugtrap03/gui/cmd/DeclareAchievedMilestoneCmd.java
+++ BugTrap/BugTrap/src/bugtrap03/gui/cmd/DeclareAchievedMilestoneCmd.java
@@ -43,66 +43,65 @@ public class DeclareAchievedMilestoneCmd implements Cmd<Project> {
     @Override
     public Project exec(TerminalScanner scan, DataModel model, User user)
             throws PermissionException, CancelException, IllegalArgumentException {
-        if (scan == null || model == null || user == null) {
-            throw new IllegalArgumentException("scan, model and user musn't be null.");
-        }
+	if (scan == null || model == null || user == null) {
+	    throw new IllegalArgumentException("scan, model and user musn't be null.");
+	}
 
-        // 1. The developer indicates that he wants to declare an achieved
-        // milestone.
-        // 2. The system shows a list of projects.
-        // 3. The developer selects a project.
-        Project proj = new GetProjectCmd().exec(scan, model, user);
+	// 1. The developer indicates that he wants to declare an achieved
+	// milestone.
+	// 2. The system shows a list of projects.
+	// 3. The developer selects a project.
+	Project proj = new GetProjectCmd().exec(scan, model, user);
 
-        // 4. The system shows a list of subsystems of the selected project.
-        // 5. The developer selects a subsystem.
-        PList<Subsystem> subsysList = model.getAllSubsystems(proj);
-        Subsystem subsys = new GetObjectOfListCmd<>(subsysList, (u -> u.getName()),
-                ((u, input) -> u.getName().equals(input))).exec(scan, model, user);
+	// 4. The system shows a list of subsystems of the selected project.
+	// 5. The developer selects a subsystem.
+	PList<Subsystem> subsysList = model.getAllSubsystems(proj);
+	Subsystem subsys = new GetObjectOfListCmd<>(subsysList, (u -> u.getName()),
+	        ((u, input) -> u.getName().equals(input))).exec(scan, model, user);
 
-        if (subsys == null) {
-            throw new IllegalArgumentException("Cancelled command.");
-        }
+	if (subsys == null) {
+	    throw new IllegalArgumentException("Cancelled command.");
+	}
 
-        // 6. The system shows the currently achieved milestones and asks for a
-        // new one.
-        scan.println("The currently achieved milestone: " + subsys.getMilestone().toString());
+	// 6. The system shows the currently achieved milestones and asks for a
+	// new one.
+	scan.println("The currently achieved milestone: " + subsys.getMilestone().toString());
 
-        // 7. The developer proposes a new achieved milestone.
+	// 7. The developer proposes a new achieved milestone.
 
-        // 8. The system updates the achieved milestone of the selected
-        // component. If necessary, the system first recursively updates the
-        // achieved milestone of all the subsystems that the component contains.
-        Milestone bugReportMilestone = null;
-        do {
-            scan.print("Enter a new milestone: (format a.b.c) ");
-            String input = scan.nextLine();
-            String[] milestoneStr = input.split("\\.");
+	// 8. The system updates the achieved milestone of the selected
+	// component. If necessary, the system first recursively updates the
+	// achieved milestone of all the subsystems that the component contains.
+	Milestone bugReportMilestone = null;
+	do {
+	    scan.print("Enter a new milestone: (format a.b.c) ");
+	    String input = scan.nextLine();
+	    String[] milestoneStr = input.split("\\.");
 
-            int nb1, nb2, nb3;
-            try {
-                nb1 = Integer.parseInt(milestoneStr[0]);
-                nb2 = Integer.parseInt(milestoneStr[1]);
-                nb3 = Integer.parseInt(milestoneStr[2]);
+	    int[] milestoneInt = new int[milestoneStr.length];
+	    try {
+		for (int i = 0; i < milestoneStr.length; i++) {
+		    milestoneInt[0] = Integer.parseInt(milestoneStr[i]);
+		}
+		bugReportMilestone = new Milestone(milestoneInt);
+		try {
+		    model.setMilestone(user, subsys, bugReportMilestone);
+		} catch (IllegalArgumentException e) {
+		    // 8a. The new achieved milestone could not be assigned due
+		    // to some constraint.
+		    // 1. The system is restored and the use case has no effect.
+		    // 2. The use case ends here.
+		    scan.println("Invalid milestone");
+		    return proj;
+		}
+	    } catch (IndexOutOfBoundsException | NumberFormatException ex) {
+		scan.println("Invalid input. Please try again using format: a.b.c...");
+	    }
+	} while (bugReportMilestone == null);
 
-                bugReportMilestone = new Milestone(nb1, nb2, nb3);
-                try {
-                    model.setMilestone(user, subsys, bugReportMilestone);
-                } catch (IllegalArgumentException e) {
-                    // 8a. The new achieved milestone could not be assigned due
-                    // to some constraint.
-                    // 1. The system is restored and the use case has no effect.
-                    // 2. The use case ends here.
-                    scan.println("Invalid milestone");
-                    return proj;
-                }
-            } catch (IndexOutOfBoundsException | NumberFormatException ex) {
-                scan.println("Invalid input. Please try again using format: a.b.c");
-            }
-        } while (bugReportMilestone == null);
-        
-        scan.println("The milestone is declared.");
-        
-        return proj;
+	scan.println("The milestone is declared.");
+
+	return proj;
     }
 
 }
