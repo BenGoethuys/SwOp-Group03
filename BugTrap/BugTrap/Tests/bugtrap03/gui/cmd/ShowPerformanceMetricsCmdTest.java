47f15e7c1a2cbb680e985906a5c416990f3a5b21
Mathias Dekempeneer <mathiasdekempeneer@gmail.com>
2016-05-11 13:31:28 +0200

    Added some tests

--- BugTrap/BugTrap/Tests/bugtrap03/gui/cmd/ShowPerformanceMetricsCmdTest.java
+++ BugTrap/BugTrap/Tests/bugtrap03/gui/cmd/ShowPerformanceMetricsCmdTest.java
@@ -1,12 +1,27 @@
-/**
- * 
- */
 package bugtrap03.gui.cmd;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayDeque;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import bugtrap03.bugdomain.HealthAlgorithm1;
+import bugtrap03.bugdomain.HealthAlgorithm2;
+import bugtrap03.bugdomain.HealthAlgorithm3;
+import bugtrap03.bugdomain.Project;
+import bugtrap03.bugdomain.Subsystem;
+import bugtrap03.bugdomain.VersionID;
+import bugtrap03.bugdomain.bugreport.BugReport;
+import bugtrap03.bugdomain.usersystem.Administrator;
+import bugtrap03.bugdomain.usersystem.Developer;
+import bugtrap03.bugdomain.usersystem.Issuer;
+import bugtrap03.gui.cmd.general.CancelException;
+import bugtrap03.model.DataModel;
+import purecollections.PList;
+import testCollection.MultiByteArrayInputStream;
+import testCollection.TerminalTestScanner;
 
 /**
  * @author Mathias
@@ -14,19 +29,93 @@ import org.junit.Test;
  */
 public class ShowPerformanceMetricsCmdTest {
 
+    static Administrator admin;
+    static DataModel model;
+    static Developer lead;
+    static Project projectA;
+    static Project projectB;
+    static Subsystem subsystemA1;
+    static Subsystem subsystemA2;
+    static Subsystem subsystemA3;
+    static Subsystem subsystemA3_1;
+    static Subsystem subsystemA3_2;
+    // static BugReport bugRep1;
+
     /**
      * @throws java.lang.Exception
      */
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
+	// Setup variables.
+	model = new DataModel();
+	lead = model.createDeveloper("ShowPerform1", "Luky", "Luke");
+	admin = model.createAdministrator("ShowPerform2", "adminT", "bie");
+	// Issuer issuer = model.createIssuer("SplitSub6", "BMW", "looks", "nice");
+	projectA = model.createProject(new VersionID(), "ShowPerform3", "Project for testing 0", lead, 500, admin);
+	projectB = model.createProject(new VersionID(), "ProjectTest1", "Project for testing 1", lead, 1000, admin);
+
+	// make subsystems
+	subsystemA1 = model.createSubsystem(admin, projectA, "SubsystemA1", "Description of susbsystem A1");
+	subsystemA2 = model.createSubsystem(admin, projectA, "SubsystemA2", "Description of susbsystem A2");
+	subsystemA3 = model.createSubsystem(admin, projectA, "SubsystemA3", "Description of susbsystem A3");
+	subsystemA3_1 = model.createSubsystem(admin, subsystemA3, "SubsystemA3.1", "Description of susbsystem A3.1");
+	subsystemA3_2 = model.createSubsystem(admin, subsystemA3, "SubsystemA3.2", "Description of susbsystem A3.2");
+
+	// bugRep1 = model.createBugReport(subsystemA3, issuer, "Used library not in repository", "title says it all.",
+	// PList.<BugReport> empty(), null, 1, false);
     }
 
     /**
-     * Test method for {@link bugtrap03.gui.cmd.ShowPerformanceMetricsCmd#exec(bugtrap03.gui.terminal.TerminalScanner, bugtrap03.model.DataModel, bugtrap03.bugdomain.usersystem.User)}.
+     * Test method for
+     * {@link bugtrap03.gui.cmd.ShowPerformanceMetricsCmd#exec(bugtrap03.gui.terminal.TerminalScanner, bugtrap03.model.DataModel, bugtrap03.bugdomain.usersystem.User)}
+     * .
+     * 
+     * @throws CancelException
+     * @throws IllegalArgumentException
      */
     @Test
-    public void testExec() {
-	fail("Not yet implemented");
+    public void testExec() throws IllegalArgumentException, CancelException {
+	ArrayDeque<String> question = new ArrayDeque<>();
+	ArrayDeque<String> answer = new ArrayDeque<>();
+	ShowPerformanceMetricsCmd cmd = new ShowPerformanceMetricsCmd();
+
+	// Setup scenario
+	question.add("Please select a developer to show the performance metrics of.");
+	question.add("Available options:");
+	question.add("0. " + lead.getUsername());
+	question.add("I choose: ");
+	answer.add("0");
+	question.add("--Developer details--");
+	question.add("name: " + lead.getFullName());
+	question.add("--Performance Metrics--");
+	question.add("- Reporting -");
+	question.add("Duplicate bug reports submitted: 0");
+	question.add("NotABug bug reports submitted: 0");
+	question.add("Bug reports submitted: 0");
+	question.add("- Leadership -");
+	question.add("ShowPerform3:\n"
+		+ "\tAlgorithm 1: HEALTHY\n"
+		+ "\tAlgorithm 2: HEALTHY\n"
+		+ "\tAlgorithm 3: HEALTHY\n"
+		+ "ProjectTest1:\n"
+		+ "\tAlgorithm 1: HEALTHY\n"
+		+ "\tAlgorithm 2: HEALTHY\n"
+		+ "\tAlgorithm 3: HEALTHY\n");
+	question.add("- Test Skills -");
+	question.add("Average lines each test: 0.0");
+	question.add("Tests submitted: 0");
+	question.add("- Problem Solving -");
+	question.add("Assigned to closed bug reports: 0");
+	question.add("Assigned to unfinished bug reports: 0");
+	question.add("Average lines each patch: 0.0");
+	question.add("Patches submitted: 0");
+
+	TerminalTestScanner scan = new TerminalTestScanner(new MultiByteArrayInputStream(answer), question);
+
+	// Execute scenario
+	Object chosen = cmd.exec(scan, model, admin);
+
+	// Test effects.
     }
 
 }
