4e073168afe05d740ac4e963df5bce2e1a3f3803
Mathias Dekempeneer <mathiasdekempeneer@gmail.com>
2016-05-11 15:46:10 +0200

    Added test

---
+++ BugTrap/BugTrap/Tests/bugtrap03/gui/cmd/general/SelectMilestoneCmdTest.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package bugtrap03.gui.cmd.general;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Mathias
+ *
+ */
+public class SelectMilestoneCmdTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    /**
+     * Test method for {@link bugtrap03.gui.cmd.general.SelectMilestoneCmd#exec(bugtrap03.gui.terminal.TerminalScanner, bugtrap03.model.DataModel, bugtrap03.bugdomain.usersystem.User)}.
+     */
+    @Test
+    public void testExec() {
+	fail("Not yet implemented");
+    }
+
+}
