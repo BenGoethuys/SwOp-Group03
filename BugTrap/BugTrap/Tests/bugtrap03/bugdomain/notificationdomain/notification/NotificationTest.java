5bf18beb9e2fd927c9e7a09dba347d120e403497
Mathias Dekempeneer <mathiasdekempeneer@gmail.com>
2016-05-11 18:13:36 +0200

    Coverage 100%

--- BugTrap/BugTrap/Tests/bugtrap03/bugdomain/notificationdomain/notification/NotificationTest.java
+++ BugTrap/BugTrap/Tests/bugtrap03/bugdomain/notificationdomain/notification/NotificationTest.java
@@ -4,15 +4,11 @@ import static org.junit.Assert.*;
 
 import java.util.GregorianCalendar;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-import bugtrap03.bugdomain.Milestone;
 import bugtrap03.bugdomain.Project;
-import bugtrap03.bugdomain.bugreport.BugReport;
 import bugtrap03.bugdomain.notificationdomain.Subject;
 import bugtrap03.bugdomain.usersystem.Developer;
-import purecollections.PList;
 
 /**
  * @author Mathias
@@ -21,26 +17,14 @@ import purecollections.PList;
 public class NotificationTest {
 
     /**
-     * @throws java.lang.Exception
+     * Test method for {@link bugtrap03.bugdomain.notificationdomain.notification.Notification#getDate()}.
      */
-    @BeforeClass
-    public static void setUpBeforeClass() throws Exception {
-	// dev4Not = new Developer("dev4Not", "devviea", "mbTestera");
-	// project4MB = new Project("Project4mb", "a project to test the mb", dev4Not, 1000);
-	// subsystem4MB = project4MB.addSubsystem("subsystem4MB", "A susbsystem to test the mb");
-	// bugreport4MB = subsystem4MB.addBugReport(dev4Not, "bugreport4MB1", "A bugreport to test the mb",
-	// new GregorianCalendar(), PList.<BugReport> empty(), new Milestone(1, 2, 3), 1, true, "triggerhappy",
-	// "stacktacktack", "error404");
-	// message4Not = "this is a test notification for mb";
-	// testNot = new BugReportNotification(message4Not, bugreport4MB, project4MB);
-    }
-
     @Test
-    public void test() {
-	Developer dev4Not = new Developer("dev4Not", "devviea", "mbTestera");
+    public void testGetDate() {
+	Developer dev4Not = new Developer("dev4No", "devviea", "mbTestera");
 	Subject test = new Project("Project4mb", "a project to test the mb", dev4Not, 1000);
 	Notification notification = new Notification("Test", test);
-	
+	assertEquals(notification.getDate(), new GregorianCalendar());
     }
 
 }
