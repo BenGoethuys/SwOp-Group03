6cf7fede35780b5b573e848e73c2e08962a50694
KwintenB <kwinten.buytaert@hotmail.com>
2016-05-11 16:54:14 +0200

    created commentary

--- BugTrap/BugTrap/src/bugtrap03/bugdomain/notificationdomain/mailboxes/ForkMailbox.java
+++ BugTrap/BugTrap/src/bugtrap03/bugdomain/notificationdomain/mailboxes/ForkMailbox.java
@@ -9,17 +9,32 @@ import bugtrap03.bugdomain.notificationdomain.notification.Notification;
  */
 public class ForkMailbox extends SubjectMailbox<Project, ProjectSubject> {
 
+    /**
+     * this constructor generates a new mailbox that represent the subscription on the forking of projects.
+     * @param projectSubject The project from which the subscription originated from.
+     */
     public ForkMailbox(ProjectSubject projectSubject) {
         super(projectSubject);
     }
 
+    /**
+     * This method returns the information about the subscription this mailbox represents.
+     * @return A string of info.
+     */
     @Override
     public String getInfo() {
         return ("You are subscribed to the forking of " + this.subject.getSubjectName());
     }
 
+    /**
+     * This method updates the notificationslist of this mailbox with a new notification about the change in the given object.
+     * @param changedObject The object of interest that has been changed.
+     *
+     * @return The new notification
+     * @throws IllegalArgumentException If the given object that changed is null;
+     */
     @Override
-    public Notification update(Project changedObject) {
+    public Notification update(Project changedObject) throws IllegalArgumentException {
         if (changedObject == null){
             throw new IllegalArgumentException("Changed object for a forking notification cannot be null");
         }
