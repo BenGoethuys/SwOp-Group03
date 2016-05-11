344983ce88fb0cfd4246ef541cdd89be03440077
Mathias Dekempeneer <mathiasdekempeneer@gmail.com>
2016-05-11 17:39:04 +0200

    Moved to the correct package

--- BugTrap/BugTrap/Tests/bugtrap03/bugdomain/notificationdomain/mailboxes/CreationMailboxTest.java
+++ BugTrap/BugTrap/Tests/bugtrap03/bugdomain/notificationdomain/mailboxes/CreationMailboxTest.java
@@ -1,10 +1,10 @@
-package bugtrap03.bugdomain.notificationdomain;
+package bugtrap03.bugdomain.notificationdomain.mailboxes;
 
 import bugtrap03.bugdomain.Milestone;
 import bugtrap03.bugdomain.Project;
 import bugtrap03.bugdomain.Subsystem;
 import bugtrap03.bugdomain.bugreport.BugReport;
-import bugtrap03.bugdomain.notificationdomain.mailboxes.CommentMailbox;
+import bugtrap03.bugdomain.notificationdomain.mailboxes.CreationMailbox;
 import bugtrap03.bugdomain.notificationdomain.notification.BugReportNotification;
 import bugtrap03.bugdomain.usersystem.Developer;
 import org.junit.BeforeClass;
@@ -16,28 +16,29 @@ import java.util.GregorianCalendar;
 import static org.junit.Assert.*;
 
 /**
- * @author Group 03
+ * @author Group03
  */
-public class CommentMailboxTest {
-    private static CommentMailbox testCMB;
+public class CreationMailboxTest {
+
+    private static CreationMailbox testCMB;
     private static BugReportNotification bugReportNotification4MB2;
 
 
-    private static Developer dev4CMB1;
+    private static Developer dev4CMB;
     private static Project project4MB;
     private static Subsystem subsystem4MB;
     private static BugReport bugreport4MB;
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
-        dev4CMB1 = new Developer("dev4CMB1", "devviea", "mbTestera");
-        project4MB = new Project("Project4mb","a project to test the mb", dev4CMB1, 1000);
+        dev4CMB = new Developer("dev4CMB", "devviea", "mbTestera");
+        project4MB = new Project("Project4mb","a project to test the mb", dev4CMB, 1000);
         subsystem4MB = project4MB.addSubsystem("subsystem4MB", "A susbsystem to test the mb");
-        bugreport4MB = subsystem4MB.addBugReport(dev4CMB1, "bugreport4MB1", "A bugreport to test the mb",
+        bugreport4MB = subsystem4MB.addBugReport(dev4CMB, "bugreport4MB1", "A bugreport to test the mb",
                 new GregorianCalendar(), PList.<BugReport>empty(), new Milestone(1,2,3),
                 1, false, "triggerhappy", "stacktacktack", "error404");
         bugReportNotification4MB2 = new BugReportNotification("this is a test notification for mb", bugreport4MB, project4MB);
-        testCMB = new CommentMailbox(project4MB);
+        testCMB = new CreationMailbox(project4MB);
     }
 
     @Test
@@ -52,7 +53,8 @@ public class CommentMailboxTest {
     public void testGetInfo() throws Exception {
         String info = testCMB.getInfo();
         assertTrue(info.contains(project4MB.getSubjectName()));
-        assertTrue(info.contains("You are subscribed to the creation of comments on "));
+        assertTrue(info.contains("You are subscribed to the creation of bugreports on "));
+        assertTrue(info.contains("and all it's subsystems."));
         assertNotEquals(null, info);
         assertNotEquals("", info);
     }
